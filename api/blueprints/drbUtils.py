06c4df41b807e93b40cefd99f2e577b1d4ca1536
Michael Benowitz <michaelbenowitz@nypl.org>
2021-07-27 14:25:40 -0400

    SFR-1221 Remove hop-by-hop headers
    WSGI servers will not re-serve "hop-by-hop" headers, which means they must be removed by the proxy service

--- api/blueprints/drbUtils.py
+++ api/blueprints/drbUtils.py
@@ -42,7 +42,7 @@ def totalCounts():
     return APIUtils.formatResponseObject(200, 'totalCounts', totalsSummary)
 
 @utils.route('/proxy', methods=['GET', 'POST', 'PUT', 'HEAD'])
-# @cross_origin(origins='http[s]?://.*nypl.org')
+@cross_origin(origins='http[s]?://.*nypl.org')
 def getProxyResponse():
     proxyUrl = request.args.get('proxy_url')
     cleanUrl = unquote_plus(proxyUrl)
@@ -67,7 +67,9 @@ def getProxyResponse():
 
     excludedHeaders = [
         'content-encoding', 'content-length', 'transfer-encoding',
-        'x-frame-options', 'referrer-policy', 'access-control-allow-origin'
+        'x-frame-options', 'referrer-policy', 'access-control-allow-origin',
+        'connection', 'keep-alive', 'public', 'proxy-authenticate',
+        'upgrade'
     ]
 
     headers = [(k, v) for (k, v) in resp.headers.items() if k.lower() not in excludedHeaders]
