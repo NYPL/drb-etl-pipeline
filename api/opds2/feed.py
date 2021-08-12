fcd517e76d7ceb97fc58a359f80dfd1b838bafd1
Michael Benowitz <michaelbenowitz@nypl.org>
2021-08-12 11:04:26 -0400

    SFR-1246 Update ODPS2 Feed class
    The OPDS2 feed class defaulted some attributes to `None` that should be lists. This updates these settings so that they will interact better in a generic sense when being constructed

--- api/opds2/feed.py
+++ api/opds2/feed.py
@@ -17,11 +17,11 @@ class Feed:
     def __init__(self, *args, **kwargs):
         self.metadata = kwargs.get('metadata', None)
         self.navigation = kwargs.get('navigation', None)
-        self.links = kwargs.get('links', None)
-        self.publications = kwargs.get('publications', None)
-        self.groups = kwargs.get('groups', None)
-        self.images = kwargs.get('images', None)
-        self.facets = kwargs.get('facets', None)
+        self.links = kwargs.get('links', [])
+        self.publications = kwargs.get('publications', [])
+        self.groups = kwargs.get('groups', [])
+        self.images = kwargs.get('images', [])
+        self.facets = kwargs.get('facets', [])
 
     def addMetadata(self, metadata):
         self.metadata = self.componentizeObject('metadata', metadata)
@@ -30,37 +30,43 @@ class Feed:
         self.addComponent('navigation', navigation)
 
     def addNavigations(self, navigations):
-        for nav in navigations: self.addNavigation(nav)
+        for nav in navigations:
+            self.addNavigation(nav)
 
     def addLink(self, link):
         self.addComponent('links', link)
 
     def addLinks(self, links):
-        for link in links: self.addLink(link)
+        for link in links:
+            self.addLink(link)
 
     def addPublication(self, publication):
         self.addComponent('publications', publication)
 
     def addPublications(self, publications):
-        for pub in publications: self.addPublication(pub)
+        for pub in publications:
+            self.addPublication(pub)
 
     def addGroup(self, group):
         self.addComponent('groups', group)
 
     def addGroups(self, groups):
-        for group in groups: self.addGroup(group)
+        for group in groups:
+            self.addGroup(group)
 
     def addImage(self, image):
         self.addComponent('images', image)
 
     def addImages(self, images):
-        for image in images: self.addImage(image)
+        for image in images:
+            self.addImage(image)
 
     def addFacet(self, facet):
         self.addComponent('facets', facet)
 
     def addFacets(self, facets):
-        for facet in facets: self.addFacet(facet)
+        for facet in facets:
+            self.addFacet(facet)
 
     def addComponent(self, name, component):
         element = getattr(self, name) 
@@ -77,7 +83,7 @@ class Feed:
         if isinstance(component, dict):
             ComponentClass = cls.COMPONENT_MAPPING[name]
             component = ComponentClass(**component)
-        
+
         return component
 
     def __dir__(self):
@@ -87,13 +93,15 @@ class Feed:
         ]
 
     def __iter__(self):
-        if len(list(filter(lambda x: x.rel == 'self' or 'self' in x.rel, self.links))) == 0:
+        if len(list(filter(
+            lambda x: x.rel == 'self' or 'self' in x.rel, self.links
+        ))) == 0:
             raise OPDS2FeedException('Link with rel of "self" must be present')
 
         for componentName in dir(self):
             component = getattr(self, componentName)
-            
-            if component is None:
+
+            if component is None or component == []:
                 continue
             if isinstance(component, list):
                 yield componentName, [dict(c) for c in component]
