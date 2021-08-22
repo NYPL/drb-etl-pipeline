68638a2d05d5f2987c1767f1670b2f4037912843
Mike Benowitz <mwbenowitz@gmail.com>
2021-08-22 11:39:46 -0400

    SFR-1271 Add API specific deployment process

--- managers/__init__.py
+++ managers/__init__.py
@@ -1,16 +1,22 @@
-from .coverManager import CoverManager
+# Managers Necessary for API, must always be available
 from .db import DBManager
-from .doabParser import DOABLinkManager
-from .gutenberg import GutenbergManager
-from .kMeans import KMeansManager
-from .oclcCatalog import OCLCCatalogManager
-from .oclcClassify import ClassifyManager
-from .nyplApi import NyplApiManager
-from .webpubManifest import WebpubManifest
-from .rabbitmq import RabbitMQManager
 from .redis import RedisManager
-from .sfrRecord import SFRRecordManager
 from .elasticsearch import ElasticsearchManager
-from .sfrElasticRecord import SFRElasticRecordManager
-from .s3 import S3Manager
-from .smartsheet import SmartSheetManager
+
+# Managers for other processes, can be skipped for API
+try:
+    from .coverManager import CoverManager
+    from .doabParser import DOABLinkManager
+    from .gutenberg import GutenbergManager
+    from .kMeans import KMeansManager
+    from .oclcCatalog import OCLCCatalogManager
+    from .oclcClassify import ClassifyManager
+    from .nyplApi import NyplApiManager
+    from .webpubManifest import WebpubManifest
+    from .rabbitmq import RabbitMQManager
+    from .sfrRecord import SFRRecordManager
+    from .sfrElasticRecord import SFRElasticRecordManager
+    from .s3 import S3Manager
+    from .smartsheet import SmartSheetManager
+except (ModuleNotFoundError, ImportError):
+        print('Unable to import module, skipping non-API managers')
--- processes/__init__.py
+++ processes/__init__.py
@@ -1,15 +1,21 @@
-from .covers import CoverProcess
-from .doab import DOABProcess
-from .gutenberg import GutenbergProcess
-from .hathiTrust import HathiTrustProcess
-from .nypl import NYPLProcess
-from .oclcCatalog import CatalogProcess
-from .oclcClassify import ClassifyProcess
-from .sfrCluster import ClusterProcess
-from .developmentSetup import DevelopmentSetupProcess
-from .s3Files import S3Process
+# API process should always be available
 from .api import APIProcess
-from .muse import MUSEProcess
-from .ingestReport import IngestReportProcess
-from .met import METProcess
-from .migration import MigrationProcess
+
+# Other processes may be skipped
+try:
+    from .covers import CoverProcess
+    from .doab import DOABProcess
+    from .gutenberg import GutenbergProcess
+    from .hathiTrust import HathiTrustProcess
+    from .nypl import NYPLProcess
+    from .oclcCatalog import CatalogProcess
+    from .oclcClassify import ClassifyProcess
+    from .sfrCluster import ClusterProcess
+    from .developmentSetup import DevelopmentSetupProcess
+    from .s3Files import S3Process
+    from .muse import MUSEProcess
+    from .ingestReport import IngestReportProcess
+    from .met import METProcess
+    from .migration import MigrationProcess
+except (ModuleNotFoundError, ImportError):
+    print('Skipping non-API processes')
