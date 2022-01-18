3b13cda3be24dacf4016531aaf1faecb0861173e
Your Name <you@example.com>
2022-01-18 16:37:26 -0500

    Extracted metadata from all catalogs

--- BeautifulSoupScript_edits.py
+++ BeautifulSoupScript_edits.py
@@ -29,53 +29,60 @@ request_urls = [
     'https://oi.uchicago.edu/research/publications/miscellaneous-publications'
 ]
 
-download_dict = {}
-key = 1
-
 
 OIC_ROOT = 'https://oi.uchicago.edu'
 
-def main():
+def main(journal_key=0):
     #Looping through each catalog to add the download links and metadata to a dictionary
     for i, url in enumerate(request_urls):
         try:
-            elem = fetchPageHTML(url)
+            if i == 2:
+                continue
+            else:
+                elem = fetchPageHTML(url)
         except Exception as e:
             print(e)
             continue
 
         soup = BeautifulSoup(elem.text, 'lxml')   #Parsing the catalog's webpage using BeautifulSoup
 
-        if i == 1:
-            metadata = parse_Pub_Alt(soup)
-            print(metadata)
-    
-        catContainer = soup.find(class_='content-inner-left')
+        if i == 1 or i == 3:
 
-        pubList = catContainer.find('ul')
+            catContainer = soup.find(class_='catalog')
 
-        #Looping through unordered list of publications
-        for pub in pubList.children:
-            #Testing to make sure pub is a <li> Tag and not an empty string
-             if not isinstance(pub, Tag):
-                 continue
-             metadata = parsePub(pub)
-             downloadLink = pub.find('a', class_= "publication ss-standard ss-download btn")
-             metadata['url'] = downloadLink.get('href', None)
-             print(metadata)
+            pubList = catContainer.find('tbody')
+
+            pubEntry = pubList.find_all('tr')
+
+            for pub in pubEntry:
+
+                print(url)
+
+                print(parse_Pub_Dict(pub))
+
+        else:
+            journal_key = 2
+
+            catContainer = soup.find(class_='content-inner-left')
+
+            pubList = catContainer.find('ul')
+
+            #Looping through unordered list of publications
+            for pub in pubList.children:
+                #Testing to make sure pub is a <li> Tag and not an empty string
+                if not isinstance(pub, Tag):
+                    continue
+                metadata = parse_Pub_Jour(pub, journal_key)
+                downloadLink = pub.find('a', class_= "publication ss-standard ss-download btn")
+                metadata['url'] = downloadLink.get('href', None)
+                print(metadata)
         '''
-        for link in soup.find_all('a', class_= "publication ss-standard ss-download btn"):
-            download_dict[key] = link.get('href')
-            key += 1
-	'''
-
-    '''
-    #Serializing download_dict to create a JSON file
-    with open("data_file.json", "w") as write_file:
-        json.dump(download_dict, write_file)
-    '''
-#Parsing the publications for metadata at the bottom of webpage
-def parsePub(pub):
+        #Serializing download_dict to create a JSON file
+        with open("data_file.json", "w") as write_file:
+            json.dump(download_dict, write_file)
+        '''
+#Parsing the journal and miscelleanous publications for metadata at the bottom of webpage
+def parse_Pub_Jour(pub, journal_key):
     pubLinks = pub.find_all('a')
     #Link should only be the link to the publication webpage
     detailLink = pubLinks[0].get('href', None)
@@ -95,35 +102,84 @@ def parsePub(pub):
 
     metadata = dataList.find_all('li')
 
-    if metadata[2].text[0:4] == 'ISBN':
+    #CHDS metadata dictionary
+    if journal_key == 2:
+        if metadata[2].text[0:4] == 'ISBN':
+            return {
+            'series': metadata[0].text,
+            'publicationInfo': metadata[1].text,
+            'isbnNumber': metadata[2].text,
+            'extent': metadata[3].text
+            }
+        else:
+            return {
+            'series': metadata[0].text,
+            'publicationInfo': metadata[1].text,
+            'extent': metadata[2].text
+            }
+    #OIDA metadata dictionary
+    elif journal_key == 7:
         return {
-       'series': metadata[0].text,
-       'publicationInfo': metadata[1].text,
-       'isbnNumber': metadata[2].text,
-       'extent': metadata[3].text
-    }
+        'series': metadata[0].text,
+        'publicationInfo': metadata[1].text,
+        'extent': metadata[2].text
+        }
+    #OIMP metadata dictionary
+    elif journal_key == 8  or journal_key == 10:
+         return {
+            'series': metadata[0].text,
+            'publicationInfo': metadata[1].text,
+            'isbnNumber(hardbook)': metadata[2].text,
+            'isbnNumber(ebook)': metadata[3].text,
+            'extent': metadata[4].text
+            }
+    #OIS metadata dictionary(Sometimes series firsts and sometimes it's last in the list)
+    elif journal_key == 11:
+        return {
+            'series': metadata[3].text,
+            'publicationInfo': metadata[0].text,
+            'isbnNumber': metadata[1].text,
+            'extent': metadata[2].text
+            }
+    elif journal_key == 13:
+        return {
+            'title': metadata[0].text,
+            'publicationInfo': metadata[1].text,
+            'isbnNumber-13': metadata[3].text,
+            'isbnNumber-10': metadata[4].text,
+            'extent': metadata[2].text
+        }
+
     else:
-        return {
-       'series': metadata[0].text,
-       'publicationInfo': metadata[1].text,
-       'extent': metadata[2].text
+        if metadata[2].text[0:4] == 'ISBN':
+            return {
+        'series': metadata[0].text,
+        'publicationInfo': metadata[1].text,
+        'isbnNumber': metadata[2].text,
+        'extent': metadata[3].text
+        }
+        else:
+            return {
+        'series': metadata[0].text,
+        'publicationInfo': metadata[1].text,
+        'extent': metadata[2].text
+        }
+
+#Parsing Dictionary catalogs
+def parse_Pub_Dict(pub):
+    pub_tds = pub.find_all('td')
+    metadata = {
+        'Volume': pub_tds[0].text,
+        'publicationInfo': pub_tds[2].text,
+        'isbnNumber': pub_tds[3].text,
+        'Extent': pub_tds[1].text
     }
-
-def parse_Pub_Alt(soup):
-    catContainer = soup.find(class_='catalog')
-
-    pubList = catContainer.find('tbody')
-
-    pubEntry = pubList.find_all('tr')
-
-    for pub in pubEntry:
-        pub_tds = pub.find_all('td')
-        return({
-            'Volume': pub_tds[0].text,
-            'publicationInfo': pub_tds[2].text,
-            'isbnNumber': pub_tds[3].text,
-            'Extent': pub_tds[1].text
-            } )
+    downloadLink = pub.find('a', class_= "publication ss-standard ss-download btn")
+    #Conditional if there is no download link for publication
+    if downloadLink != None:
+        metadata['url'] = downloadLink.get('href', None)
+    return metadata
+    
 
 if __name__ == '__main__':
     main()
