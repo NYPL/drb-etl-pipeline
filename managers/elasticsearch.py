59351e26804feb4370cba4560aff056d8ced2a50
Michael Benowitz <michaelbenowitz@nypl.org>
2022-02-01 17:52:37 -0500

    TEST Update pipeline code

--- managers/elasticsearch.py
+++ managers/elasticsearch.py
@@ -15,13 +15,17 @@ class ElasticsearchManager:
         self.index = index or os.environ.get('ELASTICSEARCH_INDEX', None)
         self.client = None
 
-    def createElasticConnection(self, host=None, port=None):
+    def createElasticConnection(self, host=None, port=None, user=None, pswd=None):
         host = host or os.environ.get('ELASTICSEARCH_HOST', None)
         port = port or os.environ.get('ELASTICSEARCH_PORT', None)
+        user = user or os.environ.get('ELASTICSEARCH_USER', None)
+        pswd = pswd or os.environ.get('ELASTICSEARCH_PSWD', None)
         timeout = int(os.environ.get('ELASTICSEARCH_TIMEOUT', 5))
 
+        creds = '{}:{}@'.format(user, pswd) if user and pswd else ''
+
         self.client = connections.create_connection(
-            hosts=['{}:{}'.format(host, port)],
+            hosts=['{}{}:{}'.format(creds, host, port)],
             timeout=timeout,
             retry_on_timeout=True,
             max_retries=3
@@ -31,31 +35,71 @@ class ElasticsearchManager:
         esIngestClient = IngestClient(self.client)
 
         self.constructLanguagePipeline(
-            esIngestClient, 'title_language_detector', 'title', 'Work title language detection'
+            esIngestClient, 'title_language_detector', 'Work title language detection',
+            field='title.'
         )
 
         self.constructLanguagePipeline(
-            esIngestClient, 'alt_title_language_detector', 'alt_titles', 'Work alt_title language detection'
+            esIngestClient, 'alt_title_language_detector', 'Work alt_title language detection',
+            prefix='_ingest._value.'
         )
 
         self.constructLanguagePipeline(
-            esIngestClient, 'edition_title_language_detector', 'title', 'Edition title language detection',
-            prefix='_ingest._value'
+            esIngestClient, 'edition_title_language_detector', 'Edition title language detection',
+            prefix='_ingest._value.',
+            field='title.'
         )
 
         self.constructLanguagePipeline(
-            esIngestClient, 'edition_sub_title_language_detector', 'sub_title', 'Edition subtitle language detection',
-            prefix='_ingest._value'
+            esIngestClient, 'edition_sub_title_language_detector', 'Edition subtitle language detection',
+            prefix='_ingest._value.',
+            field='sub_title.'
         )
 
         self.constructLanguagePipeline(
-            esIngestClient, 'edition_alt_title_language_detector', 'alt_titles', 'Edition alttitle language detection',
-            prefix='_ingest._value'
+            esIngestClient, 'subject_heading_language_detector', 'Work title language detection',
+            prefix='_ingest._value.',
+            field='heading.'
         )
 
-        self.constructLanguagePipeline(
-            esIngestClient, 'subject_heading_language_detector', 'heading', 'Work title language detection',
-            prefix='_ingest._value'
+        esIngestClient.put_pipeline(
+            id='foreach_alt_title_language_detector',
+            body={
+                'description': 'loop for parsing alt_titles',
+                'processors': [
+                    {
+                        'foreach': {
+                            'field': 'alt_titles',
+                            'processor': {
+                                'pipeline': {
+                                    'name': 'alt_title_language_detector',
+                                }
+                            }
+                        }
+                    }
+                ]
+            }
+        )
+
+        esIngestClient.put_pipeline(
+            id='edition_language_detector',
+            body={
+                'description': 'loop for parsing alt_titles',
+                'processors': [
+                    {
+                        'pipeline': {
+                            'name': 'edition_title_language_detector',
+                            'ignore_failure': True
+                        }
+                    },
+                    {
+                        'pipeline': {
+                            'name': 'edition_sub_title_language_detector',
+                            'ignore_failure': True
+                        }
+                    }
+                ]
+            }
         )
 
         esIngestClient.put_pipeline(
@@ -65,12 +109,14 @@ class ElasticsearchManager:
                 'processors': [
                     {
                         'pipeline': {
-                            'name': 'title_language_detector'
+                            'name': 'title_language_detector',
+                            'ignore_failure': True
                         }
                     },
                     {
                         'pipeline': {
-                            'name': 'alt_title_language_detector'
+                            'name': 'foreach_alt_title_language_detector',
+                            'ignore_failure': True
                         }
                     },
                     {
@@ -78,27 +124,8 @@ class ElasticsearchManager:
                             'field': 'editions',
                             'processor': {
                                 'pipeline': {
-                                    'name': 'edition_title_language_detector'
-                                }
-                            }
-                        }
-                    },
-                    {
-                        'foreach': {
-                            'field': 'editions',
-                            'processor': {
-                                'pipeline': {
-                                    'name': 'edition_sub_title_language_detector'
-                                }
-                            }
-                        }
-                    },
-                    {
-                        'foreach': {
-                            'field': 'editions',
-                            'processor': {
-                                'pipeline': {
-                                    'name': 'edition_alt_title_language_detector'
+                                    'name': 'edition_language_detector',
+                                    'ignore_failure': True
                                 }
                             }
                         }
@@ -106,9 +133,11 @@ class ElasticsearchManager:
                     {
                         'foreach': {
                             'field': 'subjects',
+                            'ignore_missing': True,
                             'processor': {
                                 'pipeline': {
-                                    'name': 'subject_heading_language_detector'
+                                    'name': 'subject_heading_language_detector',
+                                    'ignore_failure': True
                                 }
                             }
                         }
@@ -157,7 +186,7 @@ class ElasticsearchManager:
                     retries += 1
 
     @staticmethod
-    def constructLanguagePipeline(client, id, field, description, prefix=''):
+    def constructLanguagePipeline(client, id, description, prefix='', field=''):
         pipelineBody = {
             'description': description,
             'processors': [
@@ -170,28 +199,67 @@ class ElasticsearchManager:
                             }
                         },
                         'field_map': {
-                            '{}.{}'.format(prefix, field): 'text'
+                            '{}{}default'.format(prefix, field): 'text'
                         },
-                        'target_field': '{}._ml.lang_ident'.format(prefix)
+                        'target_field': '{}_ml.lang_ident'.format(prefix),
+                        'on_failure': [
+                            {
+                                'remove': {
+                                    'field': '_ml'
+                                }
+                            }
+                        ]
                     }
                 },
                 {
                     'rename': {
-                        'field': '{}.{}'.format(prefix, field),
-                        'target_field': '{}.{}.default'.format(prefix, field)
-                    }
-                },
-                {
-                    'rename': {
-                        'field': '{}._ml.lang_ident.predicted_value'.format(prefix),
-                        'target_field': '{}.{}.language'.format(prefix, field)
+                        'field': '{}_ml.lang_ident.predicted_value'.format(prefix),
+                        'target_field': '{}{}language'.format(prefix, field)
                     }
                 },
                 {
                     'set': {
-                        'field': '{}.{}.{{{}.{}.language}}'.format(prefix, field, prefix, field),
-                        'value': '{{{}.{}.default}}'.format(prefix, field),
+                        'field': 'tmp_lang',
+                        'value': '{{{{{}{}language}}}}'.format(prefix, field),
+                        'override': True
+                    }
+                },
+                {
+                    'set': {
+                        'field': 'tmp_score',
+                        'value': '{{{{{}_ml.lang_ident.prediction_score}}}}'.format(prefix),
+                        'override': True
+                    }
+                },
+                {
+                    'script': {
+                        'lang': 'painless',
+                        'source': 'ctx.supported = (["en", "de", "fr", "sp", "po", "nl", "it", "da", "ar", "zh", "el", "hi", "fa", "ja", "ru", "th"].contains(ctx.tmp_lang))'
+                    }
+                },
+                {
+                    'script': {
+                        'lang': 'painless',
+                        'source': 'ctx.threshold = Float.parseFloat(ctx.tmp_score) > 0.7'
+                    }
+                },
+                {
+                    'set': {
+                        'if': 'ctx.supported && ctx.threshold',
+                        'field': '{}{}{{{{{}{}language}}}}'.format(prefix, field, prefix, field),
+                        'value': '{{{{{}{}default}}}}'.format(prefix, field),
                         'override': False
+                    }
+                },
+                {
+                    'remove': {
+                        'field': [
+                            '{}_ml'.format(prefix),
+                            'tmp_lang',
+                            'tmp_score',
+                            'threshold',
+                            'supported'
+                        ]
                     }
                 }
             ]
