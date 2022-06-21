449bf45ff20a0f1b274090a3842a13d82bec46f7
Your Name <you@example.com>
2022-06-21 11:36:58 -0400

    Created new script for catalog links in records

---
+++ scripts/updateRecordsCatalogLink.py
@@ -0,0 +1,43 @@
+import os
+import re
+
+from model import Record
+from main import loadEnvFile
+from managers import DBManager
+
+
+def main():
+    '''
+    Updating NYPL catalog records with new link to replace text/html link
+    '''
+
+    loadEnvFile('local-qa', fileString='config/{}.yaml')
+
+    dbManager = DBManager(
+        user= os.environ.get('POSTGRES_USER', None),
+        pswd= os.environ.get('POSTGRES_PSWD', None),
+        host= os.environ.get('POSTGRES_HOST', None),
+        port= os.environ.get('POSTGRES_PORT', None),
+        db= os.environ.get('POSTGRES_NAME', None)
+    )
+
+    dbManager.generateEngine()
+
+    dbManager.createSession()
+
+    catalogRegex = r'catalog: true'
+    linkRegex = r'text/html'
+
+    for record in dbManager.session.query(Record) \
+        .filter(Record.source == 'nypl').all():
+            
+            for i in record.has_part:
+                if re.search(catalogRegex, i) != None:
+                    if re.search(linkRegex, i) != None:
+                        i.replace('text/html', 'application/html/catalog')
+                
+
+    dbManager.commitChanges()
+
+if __name__ == '__main__':
+    main()
