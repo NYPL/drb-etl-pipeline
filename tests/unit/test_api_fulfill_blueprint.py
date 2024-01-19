ec054b6c8297457589e78a0df6b777b948955dfc
Lyndsey Jane Moulds <2042238+Apophenia@users.noreply.github.com>
2024-01-18 20:33:34 -0500

    Add and update tests

--- tests/unit/test_api_fulfill_blueprint.py
+++ tests/unit/test_api_fulfill_blueprint.py
@@ -3,7 +3,7 @@ import pytest
 
 import jwt
 
-from api.blueprints.drbFulfill import itemFulfill
+from api.blueprints.drbFulfill import itemFulfill, fulfill
 from api.utils import APIUtils
 from model.postgres.link import Link
 
@@ -13,7 +13,8 @@ class TestSearchBlueprint:
     def mockUtils(self, mocker):
         return mocker.patch.multiple(
             APIUtils,
-            formatResponseObject=mocker.DEFAULT
+            formatResponseObject=mocker.DEFAULT,
+            getPresignedUrlFromObjectUrl="example.com/example.pdf"
         )
 
     @pytest.fixture
@@ -21,6 +22,7 @@ class TestSearchBlueprint:
         flaskApp = Flask('test')
         flaskApp.config['DB_CLIENT'] = 'testDBClient'
         flaskApp.config['READER_VERSION'] = 'test'
+        flaskApp.register_blueprint(fulfill)
 
         return flaskApp
 
@@ -28,7 +30,8 @@ class TestSearchBlueprint:
     def mockDB(self, mocker, monkeypatch):
         mockDB = mocker.MagicMock()
         mockDB.__enter__.return_value = mockDB
-        mockDB.fetchSingleLink.return_value = Link(flags = {"nypl_login": True})
+        mockDB.fetchSingleLink.return_value = Link(flags = {"nypl_login": True},
+                            url="https://doc-example-bucket1.s3.us-west-2.amazonaws.com/puppy.png")
         mocker.patch('api.blueprints.drbFulfill.DBClient').return_value = mockDB
 
         monkeypatch.setenv('NYPL_API_CLIENT_PUBLIC_KEY', "SomeKeyValue")
@@ -60,8 +63,24 @@ class TestSearchBlueprint:
             mockUtils['formatResponseObject'].assert_called_once_with(
                     401, 'fulfill', 'Invalid access token', headers={'WWW-Authenticate': 'Bearer'})
 
-    #def test_itemFulfill_valid(self, testApp, mockUtils, mockDB):
-    #    with testApp.test_request_context('/fulfill/12345'):
-    #        itemFulfill('12345')
+    def test_itemFulfill_invalid_iss(self, testApp, mockUtils, mockDB, mocker):
+        with testApp.test_request_context('/fulfill/12345'):
+            mocker.patch("jwt.decode", return_value={
+                "iss": "https://example.com"
+            })
+            itemFulfill('12345')
+            mockUtils['formatResponseObject'].assert_called_once_with(
+                    401, 'fulfill', 'Invalid access token', headers={'WWW-Authenticate': 'Bearer'})
 
-
+    def test_itemFulfill_redirect(self, testApp, mockDB, mocker):
+        mocker.patch("api.utils.APIUtils.getPresignedUrlFromObjectUrl", return_value="example.com/example.pdf")
+        mocker.patch("jwt.decode", return_value={
+            "iss": "https://www.nypl.org"
+        })
+        response = testApp.test_client().get(
+            '/fulfill/12345',
+            follow_redirects=False,
+            headers={'Authorization': 'Bearer Whatever'}
+        )
+        assert response.status_code == 302
+        assert response.location == "example.com/example.pdf"
