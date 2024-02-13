633b75121eb341d0c143b75f358844948a50ddea
Your Name <you@example.com>
2024-02-13 15:49:17 -0500

    Added new script to delete UMP manifest links

---
+++ scripts/deleteUMPManifestLinks.py
@@ -0,0 +1,35 @@
+import os
+
+from model import Link, Item
+from model.postgres.item import ITEM_LINKS
+from managers import DBManager
+
+def main():
+    
+    '''Deleting UMP Manifest Links from links table'''
+
+    dbManager = DBManager(
+        user= os.environ.get('POSTGRES_USER', None),
+        pswd= os.environ.get('POSTGRES_PSWD', None),
+        host= os.environ.get('POSTGRES_HOST', None),
+        port= os.environ.get('POSTGRES_PORT', None),
+        db= os.environ.get('POSTGRES_NAME', None)
+    )
+
+    dbManager.generateEngine()
+
+    dbManager.createSession()
+
+
+    for item in dbManager.session.query(Item) \
+        .filter(Item.source == 'UofM'):
+        for link in dbManager.session.query(Link) \
+            .join(ITEM_LINKS) \
+            .filter(ITEM_LINKS.c.item_id == item.id) \
+            .filter(Link.media_type == 'application/webpub+json'):   
+                dbManager.session.delete(link)
+
+    dbManager.commitChanges()
+
+if __name__ == '__main__':
+    main()
