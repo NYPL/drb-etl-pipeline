ca29a36ba09889cc42dad385c7f724f58fae46ef
Lyndsey Jane Moulds <2042238+Apophenia@users.noreply.github.com>
2024-07-23 12:23:09 -0400

    Add auth call to new OCLC 2.0 worldcat endpoint

--- managers/oclcCatalog.py
+++ managers/oclcCatalog.py
@@ -4,16 +4,21 @@ from requests.exceptions import Timeout, ConnectionError
 
 class OCLCCatalogManager:
     CATALOG_URL = 'http://www.worldcat.org/webservices/catalog/content/{}?wskey={}'
+    OCLC_AUTH_URL = 'https://oauth.oclc.org/token?scope=wcapi&grant_type=client_credentials'
+    # TODO: don't have init param oclcNo
     def __init__(self, oclcNo):
         self.oclcNo = oclcNo
         self.oclcKey = os.environ['OCLC_API_KEY']
+        self.oclcClientID = os.environ['OCLC_CLIENT_ID']
+        self.oclcClientSecret = os.environ['OCLC_CLIENT_SECRET']
         self.attempts = 0
 
+    # TODO: pass in oclcNo
     def queryCatalog(self):
         classifyResp = None
         self.attempts += 1
         catalogQuery = self.CATALOG_URL.format(self.oclcNo, self.oclcKey)
-        if self.attempts > 3: 
+        if self.attempts > 3:
             return classifyResp
 
         try:
@@ -27,6 +32,18 @@ class OCLCCatalogManager:
                 classifyResp.status_code
             ))
             return None
-    
+
         return classifyResp.text
-        
+
+    def new_query(self):
+        auth_response = requests.post(
+            self.OCLC_AUTH_URL,
+            headers={ 'Content-Type': 'application/x-www-form-urlencoded' },
+            auth=(self.oclcClientID, self.oclcClientSecret),
+        )
+
+        print(auth_response)
+        # TODO:
+        # 1. auth
+        # 2. bibs endpoint
+
