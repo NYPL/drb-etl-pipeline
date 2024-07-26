9bd43743cff2f670762d1f9c35383d5a4e8b67fa
Lyndsey Jane Moulds <2042238+Apophenia@users.noreply.github.com>
2024-07-25 23:02:24 -0400

    Add search function and integration test

--- managers/oclcCatalog.py
+++ managers/oclcCatalog.py
@@ -5,6 +5,7 @@ from requests.exceptions import Timeout, ConnectionError
 class OCLCCatalogManager:
     CATALOG_URL = 'http://www.worldcat.org/webservices/catalog/content/{}?wskey={}'
     OCLC_AUTH_URL = 'https://oauth.oclc.org/token?scope=wcapi&grant_type=client_credentials'
+    OCLC_SEARCH_URL = 'https://americas.discovery.api.oclc.org/worldcat/search/v2/'
     # TODO: don't have init param oclcNo
     def __init__(self, oclcNo):
         self.oclcNo = oclcNo
@@ -12,12 +13,15 @@ class OCLCCatalogManager:
         self.oclcClientID = os.environ['OCLC_CLIENT_ID']
         self.oclcClientSecret = os.environ['OCLC_CLIENT_SECRET']
         self.attempts = 0
+        self.token = None
+        self.tokenExpiresAt = None
 
     # TODO: pass in oclcNo
     def queryCatalog(self):
         classifyResp = None
         self.attempts += 1
         catalogQuery = self.CATALOG_URL.format(self.oclcNo, self.oclcKey)
+
         if self.attempts > 3:
             return classifyResp
 
@@ -35,15 +39,44 @@ class OCLCCatalogManager:
 
         return classifyResp.text
 
-    def new_query(self):
-        auth_response = requests.post(
-            self.OCLC_AUTH_URL,
-            headers={ 'Content-Type': 'application/x-www-form-urlencoded' },
-            auth=(self.oclcClientID, self.oclcClientSecret),
-        )
+    def getToken(self):
+        try:
+            authResp = requests.post(
+                self.OCLC_AUTH_URL,
+                headers={ 'Content-Type': 'application/x-www-form-urlencoded'},
+                auth=(self.oclcClientID, self.oclcClientSecret),
+            )
+        except (Timeout, ConnectionError):
+            print(f'Failed to retrieve token from {self.OCLC_AUTH_URL}')
+            return None
 
-        print(auth_response)
-        # TODO:
-        # 1. auth
-        # 2. bibs endpoint
+        if authResp.status_code != 200:
+            print('OCLC token retrieval failed with status {}'.format(
+                authResp.status_code
+            ))
+            return None
 
+        self.token = authResp.json()['access_token']
+        self.tokenExpiresAt = authResp.json()['expires_at']
+
+    def queryBriefBibs(self, query):
+        """Accepts a query in the form of an OCLC keyword search or fielded search"""
+        self.getToken()
+        bibsEndpoint = self.OCLC_SEARCH_URL + 'brief-bibs'
+
+        headers = {"Authorization": f"Bearer {self.token}"}
+        try:
+            bibsResp = requests.get(
+                bibsEndpoint,
+                headers=headers,
+                params={'q': query}
+            )
+        except (Timeout, ConnectionError):
+            print(f'Failed to query {bibsEndpoint} with query {query}')
+            return self.queryCatalog()
+
+        if bibsResp.status_code != 200:
+            print(f'OCLC Catalog Request failed with status {bibsResp.status_code}')
+            return None
+
+        return bibsResp.json()
