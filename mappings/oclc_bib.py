bb0d2dd6e1f413709bca93c14d25fc9617356b9f
kyle <kylejacksonvillegas@nypl.org>
2024-08-22 14:36:21 -0400

    fixing unit tests

--- mappings/oclc_bib.py
+++ mappings/oclc_bib.py
@@ -11,7 +11,7 @@ def map_oclc_bib_to_record(oclc_bib, related_oclc_numbers=[]) -> Record:
         cluster_status=False,
         source='oclcClassify',
         # TODO: should this be an owi number? 
-        source_id=f'{oclc_bib['oclcNumber']}|oclc',
+        source_id=f"{oclc_bib['oclcNumber']}|oclc",
         title=oclc_bib['title'],
         # TODO: explicitly request subject search facet
         subjects=[],
@@ -22,10 +22,10 @@ def map_oclc_bib_to_record(oclc_bib, related_oclc_numbers=[]) -> Record:
         # TODO: previously we weren't pulling in dates but we could here
         dates=[],
         # TODO: should we include merged OCLC numbers? 
-        identifiers=[f'{oclc_bib['oclcNumber']}|oclc'] + \
-            map(lambda issn: f'{issn}|issn', oclc_bib['issns']) if oclc_bib['issns'] else [] + \
-            map(lambda isbn: f'{isbn}|issn', oclc_bib['isbns']) if oclc_bib['isbns'] else [] + \
-            map(lambda oclc_number: f'{oclc_number}|oclc', related_oclc_numbers),
+        identifiers=[f"{oclc_bib['oclcNumber']}|oclc"] + \
+            map(lambda issn: f"{issn}|issn", oclc_bib['issns']) if oclc_bib['issns'] else [] + \
+            map(lambda isbn: f"{isbn}|issn", oclc_bib['isbns']) if oclc_bib['isbns'] else [] + \
+            map(lambda oclc_number: f"{oclc_number}|oclc", related_oclc_numbers),
         publisher=[oclc_bib['publisher']],
         date_created=datetime.now(timezone.utc).replace(tzinfo=None),
         date_modified=datetime.now(timezone.utc).replace(tzinfo=None)
