82d5186133499e8e39f78805303d9f88153b739d
kyle <kylejacksonvillegas@nypl.org>
2024-08-22 14:40:53 -0400

    fixing mapping

--- mappings/oclc_bib.py
+++ mappings/oclc_bib.py
@@ -24,7 +24,7 @@ def map_oclc_bib_to_record(oclc_bib, related_oclc_numbers=[]) -> Record:
         # TODO: should we include merged OCLC numbers? 
         identifiers=[f"{oclc_bib['oclcNumber']}|oclc"] + \
             map(lambda issn: f"{issn}|issn", oclc_bib['issns']) if oclc_bib['issns'] else [] + \
-            map(lambda isbn: f"{isbn}|issn", oclc_bib['isbns']) if oclc_bib['isbns'] else [] + \
+            map(lambda isbn: f"{isbn}|isbn", oclc_bib['isbns']) if oclc_bib['isbns'] else [] + \
             map(lambda oclc_number: f"{oclc_number}|oclc", related_oclc_numbers),
         publisher=[oclc_bib['publisher']],
         date_created=datetime.now(timezone.utc).replace(tzinfo=None),
