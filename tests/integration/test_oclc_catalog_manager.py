6851b887db2cb4f084edcf922a8bb2605715549c
kyle <kylejacksonvillegas@nypl.org>
2024-09-11 12:07:16 -0400

    updating test

--- tests/integration/test_oclc_catalog_manager.py
+++ tests/integration/test_oclc_catalog_manager.py
@@ -10,10 +10,11 @@ class TestOCLCCatalogManager:
 
     def test_query_bibs(self, test_instance: OCLCCatalogManager):
         bibs = test_instance.query_bibs('ti:The Waves')
-        
+
         assert len(bibs) > 0
 
     def test_query_other_editions(self, test_instance: OCLCCatalogManager):
         related_oclc_numbers = test_instance.get_related_oclc_numbers(1)
+        
         assert len(related_oclc_numbers) > 0
         assert 1 not in set(related_oclc_numbers)
