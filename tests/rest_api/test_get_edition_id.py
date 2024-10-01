804eca91aa331bf683c50a17606ffc749bde48c1
ayan <100955969+ayan1229@users.noreply.github.com>
2024-10-01 16:08:12 -0400

    SFR-2181: get edition id

---
+++ tests/rest_api/test_get_edition_id.py
@@ -0,0 +1,23 @@
+import requests
+import pytest
+
+BASE_URL = "https://drb-api-qa.nypl.org"
+
+def test_get_collection():
+
+    url = BASE_URL + "/edition/1982731"
+    response = requests.get(url)
+
+    assert response.status_code is not None
+    assert response.status_code == 200, (
+        f"API call failed.\n"
+        f"Expected status code: {200}\n"
+        f"Actual status code: {response.status_code}\n"
+        f"URL: {url}\n"
+        f"Response text: {response.text[:100]}..."
+    )
+
+    response_json = response.json()
+    
+    assert response_json is not None
+
