6840f9b75b7e72bed3c09bb91652aa312165cfb2
kyle <kylejacksonvillegas@nypl.org>
2024-10-16 12:20:24 -0400

    SFR-2256: Refactoring API Process

--- processes/api.py
+++ processes/api.py
@@ -1,28 +1,28 @@
-from .core import CoreProcess
 from api.app import FlaskAPI
 from logger import createLog
+from managers import DBManager, ElasticsearchManager, RedisManager
 
 logger = createLog(__name__)
 
 
-class APIProcess(CoreProcess):
-    def __init__(self, *args):
-        super(APIProcess, self).__init__(*args[:4])
-
-        # ElasticSearch Connection
-        self.createElasticConnection()
-
-        # Generate PostgreSQL engine
-        self.generateEngine()
-
-        # Redis Connection
-        self.createRedisClient()
-
-        self.api = FlaskAPI(self.engine, self.redisClient)
+class APIProcess():
+    def __init__(self):
+        self.db_manager = DBManager()
+        self.elastic_search_manager = ElasticsearchManager()
+        self.redis_manager = RedisManager()
 
     def runProcess(self):
-        logger.info('Starting API...')
+        try:
+            logger.info('Starting API...')
+            
+            db_engine = self.db_manager.generateEngine()
+            redis_client = self.redis_manager.createRedisClient()
+            self.elastic_search_manager.createElasticConnection()
+            
+            api = FlaskAPI(db_engine, redis_client)
 
-        self.api.createErrorResponses()
-
-        self.api.run()
+            api.createErrorResponses()
+            api.run()
+        except Exception as e:
+            logger.exception('Failed to start API')
+            raise e
