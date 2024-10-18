e086f24b266c32989821210596db24d6b5c45252
Kyle Villegas <86266231+kylevillegas93@users.noreply.github.com>
2024-10-18 13:07:42 -0400

    SFR-2261: Refactoring db migration process (#404)

---
+++ processes/db_migration.py
@@ -0,0 +1,20 @@
+from alembic import config
+from logger import createLog
+
+logger = createLog(__name__)
+
+
+class MigrationProcess():
+    def __init__(self, *args):
+        self.options = args[6]
+
+    def runProcess(self):
+        try:
+            logger.info('Running database migration')
+
+            alembic_args = ['--raiseerr'] + self.options
+
+            config.main(argv=alembic_args)
+        except Exception as e:
+            logger.exception('Failed to run database migration')
+            raise e
