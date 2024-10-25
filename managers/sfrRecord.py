beec0a18d501275743cc85f5116a12c21af93a75
kyle <kylejacksonvillegas@nypl.org>
2024-10-25 16:17:55 -0400

    SFR-2285: Fix has part item build

--- managers/sfrRecord.py
+++ managers/sfrRecord.py
@@ -258,8 +258,10 @@ class SFRRecordManager:
         editionData['dcdw_uuids'].append(rec.uuid.hex)
 
     def buildItems(self, editionData, rec, itemContributors):
+        max_part_number = max(int(item.split('|')[0]) for item in rec.has_part)
+
         startPos = len(editionData['items']) - 1
-        editionData['items'].extend([None] * len(rec.has_part))
+        editionData['items'].extend([None] * max_part_number)
 
         for item in rec.has_part:
             no, uri, source, linkType, flags = tuple(item.split('|'))
