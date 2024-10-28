138411fc494774f0619317089321088c58d9fc36
Kyle Villegas <86266231+kylevillegas93@users.noreply.github.com>
2024-10-28 14:17:16 -0400

    SFR-2283: Refactoring catalog process (#418)

--- processes/frbr/catalog.py
+++ processes/frbr/catalog.py
@@ -24,72 +24,57 @@ class CatalogProcess(CoreProcess):
 
         self.oclcCatalogManager = OCLCCatalogManager()
 
-    def runProcess(self, max_attempts: int=3):
-        self.receiveAndProcessMessages(max_attempts=max_attempts)
+    def runProcess(self, max_attempts: int=4):
+        self.process_catalog_messages(max_attempts=max_attempts)
 
         self.saveRecords()
         self.commitChanges()
 
-    def receiveAndProcessMessages(self, max_attempts: int=3):
-        attempts = 1
+        logger.info(f'Saved {len(self.records)} catalog records')
 
-        while True:
-            msgProps, _, msgBody = self.getMessageFromQueue(os.environ['OCLC_QUEUE'])
+    def process_catalog_messages(self, max_attempts: int=4):
+        for attempt in range(0, max_attempts):
+            wait_time = 60 * attempt
 
-            if msgProps is None:
-                if attempts <= max_attempts:
-                    waitTime = 60 * attempts
+            if wait_time:
+                logger.info(f'Waiting {wait_time}s for OCLC catalog messages')
+                sleep(wait_time)
 
-                    logger.info(f'Waiting {waitTime}s for OCLC catalog messages')
+            while message := self.getMessageFromQueue(os.environ['OCLC_QUEUE']):
+                message_props, _, message_body = message
 
-                    sleep(waitTime)
-
-                    attempts += 1
-
-                    continue
-                else:
-                    logger.info('Exiting OCLC catalog process - no more messages.')
+                if not message_props or not message_body:
                     break
 
-            attempts = 1
+                self.process_catalog_message(message_body)
+                self.acknowledgeMessageProcessed(message_props.delivery_tag)
 
-            self.processCatalogQuery(msgBody)
-            self.acknowledgeMessageProcessed(msgProps.delivery_tag)
-
-    def processCatalogQuery(self, msgBody):
-        message = json.loads(msgBody)
-        oclcNo = message.get('oclcNo')
-        owiNo = message.get('owiNo')
+    def process_catalog_message(self, message_body):
+        message = json.loads(message_body)
+        oclc_number = message.get('oclcNo')
+        owi_number = message.get('owiNo')
         
-        catalogXML = self.oclcCatalogManager.query_catalog(oclcNo)
+        catalog_record = self.oclcCatalogManager.query_catalog(oclc_number)
 
-        if not catalogXML:
-            logger.warning(f'Unable to get catalog XML for OCLC number {oclcNo}')
+        if not catalog_record:
+            logger.warning(f'Unable to get catalog record for OCLC number {oclc_number}')
             return
         
-        self.parseCatalogRecord(catalogXML, oclcNo, owiNo)
-        logger.info(f'Processed OCLC catalog query for OCLC number {oclcNo}')
+        self.parse_catalog_record(catalog_record, oclc_number, owi_number)
 
-    def parseCatalogRecord(self, catalogXML, oclcNo, owiNo):
+    def parse_catalog_record(self, catalog_record, oclc_number, owi_number):
         try:
-            parseMARC = etree.fromstring(catalogXML.encode('utf-8'))
-        except etree.XMLSyntaxError as e:
-            logger.error(f'OCLC catalog MARC xml is invalid for OCLC number: {oclcNo}')
-            logger.debug(e)
+            parsed_marc_xml = etree.fromstring(catalog_record.encode('utf-8'))
+        except Exception:
+            logger.exception(f'Unable to parse OCLC catalog MARC XML for OCLC number: {oclc_number}')
             return
 
-        catalogRec = CatalogMapping(
-            parseMARC,
-            {'oclc': 'http://www.loc.gov/MARC21/slim'},
-            {}
-        )
+        catalog_record_mapping = CatalogMapping(parsed_marc_xml, { 'oclc': 'http://www.loc.gov/MARC21/slim' }, {})
 
         try:
-            catalogRec.applyMapping()
-            catalogRec.record.identifiers.append('{}|owi'.format(owiNo))
-            self.addDCDWToUpdateList(catalogRec)
-        except Exception as e:
-            logger.error(
-                f'Unable to parse OCLC catalog record with id {catalogRec.record.source_id} due to {e}'
-            )
-            logger.debug(e)
+            catalog_record_mapping.applyMapping()
+            catalog_record_mapping.record.identifiers.append('{}|owi'.format(owi_number))
+            
+            self.addDCDWToUpdateList(catalog_record_mapping)
+        except Exception:
+            logger.exception(f'Unable to map OCLC catalog record for OCLC number: {oclc_number}')
