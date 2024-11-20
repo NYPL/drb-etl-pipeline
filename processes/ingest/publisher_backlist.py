f5b2ddc647d75a04222a4d06a7d1f987ad2d726b
Dmitri <you@example.com>
2024-11-20 16:23:23 -0500

    Added filter based on modified time for records

--- processes/ingest/publisher_backlist.py
+++ processes/ingest/publisher_backlist.py
@@ -13,7 +13,7 @@ class PublisherBacklistProcess(CoreProcess):
         self.limit = (len(args) >= 5 and args[4] and args(4) <= 100) or None
         self.offset = (len(args) >= 6 and args[5]) or None
 
-        self.s3Bucket = os.environ['FILE_BUCKET']
+        self.s3_bucket = os.environ['FILE_BUCKET']
         self.createS3Client()
 
         self.publisher_backlist_service = PublisherBacklistService()
@@ -30,7 +30,7 @@ class PublisherBacklistProcess(CoreProcess):
             elif self.process == 'custom':
                 records = self.publisher_backlist_service.get_records(start_timestamp=self.ingestPeriod, offset=self.offset, limit=self.limit)
             else: 
-                logger.warning(f'Unknown NYPL ingestion process type {self.process}')
+                logger.warning(f'Unknown Publisher Backlist ingestion process type {self.process}')
                 return
             
             for record in records:
@@ -39,11 +39,12 @@ class PublisherBacklistProcess(CoreProcess):
             self.saveRecords()
             self.commitChanges()
 
-            logger.info(f'Ingested {len(self.records)} UofM records')
+            logger.info(f'Ingested {len(self.records)} Publisher Backlist records')
 
         except Exception as e:
-            logger.exception('Failed to run Pub Backlist process')
+            logger.exception('Failed to run Publisher Backlist process')
             raise e   
         finally:
             self.close_connection()
+
     
--- services/sources/publisher_backlist_service.py
+++ services/sources/publisher_backlist_service.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta, timezone
 import os
 import requests
 import json
+import urllib.parse
 from typing import Optional
 
 from logger import create_log
@@ -11,7 +12,7 @@ from .source_service import SourceService
 
 logger = create_log(__name__)
 
-
+BASE_URL = "https://api.airtable.com/v0/appBoLf4lMofecGPU/Publisher%20Backlists%20%26%20Collections%20%F0%9F%93%96?view=UofMichigan%20Press"
 class PublisherBacklistService(SourceService):
     def __init__(self):
         self.airtable_auth_token = os.environ.get('AIRTABLE_KEY', None)
@@ -20,22 +21,22 @@ class PublisherBacklistService(SourceService):
 
     def get_records(
         self,
-        full_import: bool=False, # get all records from Airtable
+        full_import: bool=False, 
         start_timestamp: datetime=None,
-        offset: Optional[int]=None, # start at specific position in paginated results 
+        offset: Optional[int]=None,
         limit: Optional[int]=None
     ) -> list[UofMMapping]:
         
-        json_data = self.get_publisher_backlist_records(full_import, start_timestamp, offset, limit)
+        array_json_records = self.get_records_json(full_import, start_timestamp, offset, limit)
 
         array_index = 0
-        while array_index != len(json_data):
-            for dict_index in range(0, len(json_data['records'])):
-                metaDict = json_data['records'][dict_index]
+        while array_index != len(array_json_records):
+            for dict_index in range(0, len(array_json_records[array_index]['records'])):
+                meta_dict = array_json_records[array_index]['records'][dict_index]
                 raise Exception
             array_index += 1
 
-    def get_publisher_backlist_records(self, full_import=False, start_timestamp=None, offset=None, limit=None):
+    def get_records_json(self, full_import=False, start_timestamp=None, offset=None, limit=None):
 
         if offset == None:
             limit = 100
@@ -44,22 +45,48 @@ class PublisherBacklistService(SourceService):
 
         if not full_import:
             if start_timestamp:
-                response = requests.get(f'https://api.airtable.com/v0/appBoLf4lMofecGPU/Publisher%20Backlists%20%26%20Collections%20%F0%9F%93%96?view=UofMichigan%20Press&pageSize={limit}', headers=headers)
-                return response.json()
+                start_date_time_str = start_date_time.strftime("%Y-%m-%d %H:%M:%S.%f")
+                start_date_time_encoded = urllib.parse.quote(start_date_time_str)
+                filter_by_formula = f"OR(IS_SAME(%7BLast%20Modified%7D,%20%22{start_date_time_encoded}%22,%20%22second%22),%20IS_AFTER(%7BLast%20Modified%7D,%20%22{start_date_time_encoded}%22))"
+
+                response = requests.get(f'{BASE_URL}&filterByFormula={filter_by_formula}&pageSize={limit}', headers=headers)
+                response_json = response.json()
+                array_json = [response_json]
+
+                while 'offset' in response_json:
+                    offset = response_json['offset']
+                    response = requests.get(f'{BASE_URL}&filterByFormula={filter_by_formula}&pageSize={limit}&offset={offset}', headers=headers)
+                    response_json = response.json()
+                    array_json.append(response_json)
+
+                return array_json
             else:
                 start_date_time = datetime.now(timezone.utc).replace(tzinfo=None) - timedelta(hours=24)
-                response = requests.get(f'https://api.airtable.com/v0/appBoLf4lMofecGPU/Publisher%20Backlists%20%26%20Collections%20%F0%9F%93%96?view=UofMichigan%20Press&pageSize={limit}', headers=headers)
-                return response.json
+                start_date_time_str = start_date_time.strftime("%Y-%m-%d %H:%M:%S.%f")
+                start_date_time_encoded = urllib.parse.quote(start_date_time_str)
+                filter_by_formula = f"OR(IS_SAME(%7BLast%20Modified%7D,%20%22{start_date_time_encoded}%22,%20%22second%22),%20IS_AFTER(%7BLast%20Modified%7D,%20%22{start_date_time_encoded}%22))"
+                
+                response = requests.get(f'{BASE_URL}&filterByFormula={filter_by_formula}&pageSize={limit}', headers=headers)
+                response_json = response.json()
+                array_json = [response_json]
+
+                while 'offset' in response_json:
+                    offset = response_json['offset']
+                    response = requests.get(f'{BASE_URL}&filterByFormula={filter_by_formula}&pageSize={limit}&offset={offset}', headers=headers)
+                    response_json = response.json()
+                    array_json.append(response_json)
+
+                return array_json
         else:
-            response = requests.get(f'https://api.airtable.com/v0/appBoLf4lMofecGPU/Publisher%20Backlists%20%26%20Collections%20%F0%9F%93%96?view=UofMichigan%20Press', headers=headers)
+            response = requests.get(f'{BASE_URL}&pageSize={limit}', headers=headers)
             response_json = response.json()
             array_json = [response_json]
+
             while 'offset' in response_json:
                 offset = response_json['offset']
-                response = requests.get(f'https://api.airtable.com/v0/appBoLf4lMofecGPU/Publisher%20Backlists%20%26%20Collections%20%F0%9F%93%96?view=UofMichigan%20Press&pageSize={limit}&offset={offset}', headers=headers)
+                response = requests.get(f'{BASE_URL}&pageSize={limit}&offset={offset}', headers=headers)
                 response_json = response.json()
                 array_json.append(response_json)
 
             return array_json
-
-
+        
