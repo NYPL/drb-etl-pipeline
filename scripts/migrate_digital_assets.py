f1e5bc4654c94a92c305f67780def7968525646a
kyle <kylejacksonvillegas@nypl.org>
2024-11-22 16:08:09 -0500

    simplifying tings

--- scripts/migrate_digital_assets.py
+++ scripts/migrate_digital_assets.py
@@ -9,7 +9,7 @@ from google.oauth2.service_account import Credentials
 
 s3 = boto3.client('s3')
 
-S3_BUCKET_NAME = 'ump-pdf-repository'
+S3_BUCKET = 'ump-pdf-repository'
 SERVICE_ACCOUNT_FILE = 'service-account.json'
 FOLDER_ID = '1gRgJiPWnFhMu56KL8bKorHepF1BLElOL'
 
@@ -21,42 +21,39 @@ def create_drive_service(service_account_file):
     return build('drive', 'v3', credentials=credentials)
 
 
-def upload_to_google_drive(service, file_name, file_data):
-    media = MediaIoBaseUpload(io.BytesIO(file_data), mimetype='application/octet-stream')
-    file_metadata = {
-        'name': file_name,
-        'parents': [FOLDER_ID]
-    }
-    file = service.files().create(
-        body=file_metadata,
-        media_body=media,
+def upload_to_google_drive(file_name, file_data):
+    drive_service = create_drive_service(drive_service)
+    
+    file = drive_service.files().create(
+        body={ 'name': file_name, 'parents': [FOLDER_ID] },
+        media_body=MediaIoBaseUpload(io.BytesIO(file_data), mimetype='application/octet-stream'),
         supportsAllDrives=True,
         fields='id'
     ).execute()
+    
     return file.get('id')
 
 
-def s3_to_google_drive(s3_bucket, service_account_file):
-    drive_service = create_drive_service(service_account_file)
+def move_s3_files_to_google_drive(s3_bucket):
+    list_objects_response = s3.list_objects_v2(Bucket=s3_bucket)
     
-    response = s3.list_objects_v2(Bucket=s3_bucket)
-    if 'Contents' not in response:
+    if 'Contents' not in list_objects_response:
         print(f'No files found in S3 bucket {s3_bucket}')
         return
 
-    for obj in response['Contents']:
-        file_key = obj['Key']
+    for object in list_objects_response['Contents']:
+        file_key = object['Key']
         print(f'Processing file: {file_key}')
         
         s3_object = s3.get_object(Bucket=s3_bucket, Key=file_key)
         file_data = s3_object['Body'].read()
         
         try:
-            drive_file_id = upload_to_google_drive(drive_service, file_key, file_data)
+            drive_file_id = upload_to_google_drive(file_key, file_data)
             print(f'Uploaded {file_key} to Google Drive with ID: {drive_file_id}')
         except Exception as e:
             print(f'Failed to upload {file_key} to Google Drive: {e}')
 
 
 if __name__ == '__main__':
-    s3_to_google_drive(S3_BUCKET_NAME, SERVICE_ACCOUNT_FILE)
+    move_s3_files_to_google_drive(S3_BUCKET)
