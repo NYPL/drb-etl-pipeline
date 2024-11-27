23d8be304b4b7e134070070ed19525dddc3a5db8
Lyndsey Jane Moulds <2042238+Apophenia@users.noreply.github.com>
2024-11-27 10:29:00 -0500

    logging and exception handling for google integration

--- processes/util/google_integration.py
+++ processes/util/google_integration.py
@@ -7,6 +7,10 @@ from google.oauth2.service_account import Credentials
 
 SERVICE_ACCOUNT_FILE = os.environ['GOOGLE_SERVICE_ACCOUNT_FILE']
 
+from logger import create_log
+
+logger = create_log(__name__)
+
 def create_drive_service(service_account_info):
     scopes = ['https://www.googleapis.com/auth/drive']
     credentials = Credentials.from_service_account_info(service_account_info, scopes=scopes)
@@ -24,11 +28,12 @@ def get_drive_file(file_id: str) -> BytesIO:
         done = False
         while done is False:
             status, done = downloader.next_chunk()
-            print(f"Download {int(status.progress() * 100)}.")
 
     except HttpError as error:
-        print(f"An error occurred when downloading Drive file {file_id}: {error}")
+        logger.warning(f"HTTP error occurred when downloading Drive file {file_id}: {error}")
         file = None
+    except Exception as err:
+        logger.warning(f"Unexpected {type(err)=} occurred when downloading drive file {file_id}: {error}")
 
     return file
 
