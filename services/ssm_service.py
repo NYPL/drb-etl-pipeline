b6a89e0b58481c22ccb992bd9826976992d9742c
Lyndsey Jane Moulds <2042238+Apophenia@users.noreply.github.com>
2024-12-06 17:06:24 -0500

    Add beginning of SSM service file

---
+++ services/ssm_service.py
@@ -0,0 +1,26 @@
+import os
+import boto3
+from typing import Optional
+
+from logger import create_log
+
+logger = create_log(__name__)
+
+ssm_client = boto3.client('ssm',
+            aws_access_key_id=os.environ.get('AWS_ACCESS', None),
+            aws_secret_access_key=os.environ.get('AWS_SECRET', None),
+            region_name=os.environ.get('AWS_REGION', None)
+        )
+
+def get_parameter(parameter_name: str) -> Optional[dict]:
+    try:
+        response = ssm_client.get_parameter(
+            Name=parameter_name,
+            WithDecryption=True
+        )
+
+    except Exception as err:
+        logger.exception(f"Parameter store retrieval failed")
+        return None
+
+    return response
