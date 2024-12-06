ae599e28dc60b4646522b3542e829af4af64be70
Lyndsey M. <2042238+Apophenia@users.noreply.github.com>
2024-12-06 11:09:27 -0500

    Fix declarative_base() import warning (#467)

--- model/postgres/base.py
+++ model/postgres/base.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timezone
 from sqlalchemy import Column, DateTime
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
