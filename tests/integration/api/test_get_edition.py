a16745b1775e19a8963ef5c265c50ab3f64b07d1
ayan <100955969+ayan1229@users.noreply.github.com>
2024-12-18 10:54:05 -0500

    SFR-2311: API Edge Test Cases

--- tests/integration/api/test_get_edition.py
+++ tests/integration/api/test_get_edition.py
@@ -3,7 +3,7 @@ from .utils import assert_response_status
 import requests
 
 
-def test_get_collection():
+def test_get_edition():
     url =  API_URL + '/edition/1982731'
     response = requests.get(url)
 
@@ -14,3 +14,23 @@ def test_get_collection():
     
     assert response_json is not None
 
+def test_get_edition_non_existent_id():
+    url = API_URL + '/edition/00000000-0000-0000-0000-000000000000'
+    response = requests.get(url)
+    assert_response_status(url, response, 400)
+
+def test_get_edition_malformed_id():
+    url = API_URL + '/edition/invalid_id_format'
+    response = requests.get(url)
+    assert_response_status(url, response, 400)
+
+def test_get_edition_empty_id():
+    url = API_URL + '/edition/'
+    response = requests.get(url)
+    assert_response_status(url, response, 404)
+
+def test_get_edition_special_characters():
+    url = API_URL + '/edition/%$@!*'
+    response = requests.get(url)
+    assert_response_status(url, response, 400)
+    
