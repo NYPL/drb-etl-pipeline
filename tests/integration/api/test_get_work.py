b0170b9d188951eef83bb0b674cc5c65f15071c7
Shejanul Ayan Islam <100955969+ayan1229@users.noreply.github.com>
2024-12-19 11:13:43 -0500

    SFR-2311: API Edge Test Cases (#480)
    * SFR-2311: API Edge Test Cases
    
    * parameterize tests
    
    * rename search file

--- tests/integration/api/test_get_work.py
+++ tests/integration/api/test_get_work.py
@@ -1,16 +1,22 @@
+import pytest
+import requests
 from .constants import API_URL
 from .utils import assert_response_status
-import requests
 
-
-def test_get_collection():
-    url = API_URL + '/work/701c5f00-cd7a-4a7d-9ed1-ce41c574ad1d'
+@pytest.mark.parametrize("endpoint, expected_status", [
+    ("/work/701c5f00-cd7a-4a7d-9ed1-ce41c574ad1d", 200),
+    ("/work/00000000-0000-0000-0000-000000000000", 404),
+    ("/work/invalid_id_format", 400),
+    ("/work/", 404),
+    ("/work/%$@!*", 400)
+])
+def test_get_work(endpoint, expected_status):
+    url = API_URL + endpoint
     response = requests.get(url)
 
     assert response.status_code is not None
-    assert_response_status(url, response, 200)
+    assert_response_status(url, response, expected_status)
 
-    response_json = response.json()
-    
-    assert response_json is not None
-
+    if expected_status == 200:
+        response_json = response.json()
+        assert response_json is not None
