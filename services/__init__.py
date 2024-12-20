d5c7a7e131b2d18a35877ffd15f42603294c8269
Lyndsey Jane Moulds <2042238+Apophenia@users.noreply.github.com>
2024-12-20 15:57:58 -0500

    Refactor Drive service into a class
    Refactor drive service into a class & only return param from
    SSM service

--- services/__init__.py
+++ services/__init__.py
@@ -1,2 +1,3 @@
 from .sources.nypl_bib_service import NYPLBibService
 from .sources.publisher_backlist_service import PublisherBacklistService
+from .google_drive_service import GoogleDriveService
--- services/google_drive_service.py
+++ services/google_drive_service.py
@@ -2,39 +2,57 @@ from io import BytesIO
 import json
 import os
 from typing import Optional
+from urllib.parse import urlparse, parse_qs
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseDownload
 from googleapiclient.errors import HttpError
 from google.oauth2.service_account import Credentials
+from services.ssm_service import get_parameter
 
-SERVICE_ACCOUNT_FILE = os.environ['GOOGLE_SERVICE_ACCOUNT_FILE']
 
 from logger import create_log
 
 logger = create_log(__name__)
 
-service_account_info = json.loads(SERVICE_ACCOUNT_FILE)
-scopes = ['https://www.googleapis.com/auth/drive']
-credentials = Credentials.from_service_account_info(service_account_info, scopes=scopes)
+class GoogleDriveService:
+    def __init__(self):
+        if os.environ['ENVIRONMENT'] == 'production':
+            SERVICE_ACCOUNT_FILE = get_parameter('arn:aws:ssm:us-east-1:946183545209:parameter/drb/production/google-drive-service-key')
+        else:
+            SERVICE_ACCOUNT_FILE = get_parameter('arn:aws:ssm:us-east-1:946183545209:parameter/drb/qa/google-drive-service-key')
+        self.service_account_info = json.loads(SERVICE_ACCOUNT_FILE)
+        self.scopes = ['https://www.googleapis.com/auth/drive',
+        'https://www.googleapis.com/auth/drive.file',
+        'https://www.googleapis.com/auth/drive.metadata']
+        self.credentials = Credentials.from_service_account_info(self.service_account_info, scopes=self.scopes)
+        self.drive_service = build('drive', 'v3', credentials=self.credentials)
 
-drive_service = build('drive', 'v3', credentials=credentials)
+    def get_drive_file(self, file_id: str) -> Optional[BytesIO]:
+        request = self.drive_service.files().get_media(fileId=file_id)
+        file = BytesIO()
 
-def get_drive_file(file_id: str) -> Optional[BytesIO]:
-    request = drive_service.files().get_media(fileId=file_id)
-    file = BytesIO()
+        try:
+            downloader = MediaIoBaseDownload(file, request)
 
-    try:
-        downloader = MediaIoBaseDownload(file, request)
+            done = False
+            while not done:
+                status, done = downloader.next_chunk()
 
-        done = False
-        while not done:
-            status, done = downloader.next_chunk()
+        except HttpError as error:
+            logger.warning(f"HTTP error occurred when downloading Drive file {file_id}: {error}")
+            return None
+        except Exception as err:
+            logger.exception(f"Error occurred when downloading Drive file {file_id}")
+            return None
 
-    except HttpError as error:
-        logger.warning(f"HTTP error occurred when downloading Drive file {file_id}: {error}")
-        return None
-    except Exception as err:
-        logger.exception(f"Error occurred when downloading Drive file {file_id}")
-        return None
+        return file
 
-    return file
+    def get_file_metadata(self, file_id: str) -> Optional[str]:
+        # supportsAllDrives=True required as of 12/24 despite deprecation
+        request = self.drive_service.files().get(fileId=file_id, supportsAllDrives=True)
+        meta = request.execute()
+        return meta
+
+    @staticmethod
+    def id_from_url(url: str) -> Optional[str]:
+        return parse_qs(urlparse(url).query)['id'][0]
