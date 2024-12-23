c3a9f22995da15fcc455fa175d6ba6f2bb431bbc
kyle <kylejacksonvillegas@nypl.org>
2024-12-23 17:44:56 -0500

    eegh

--- services/sources/publisher_backlist_service.py
+++ services/sources/publisher_backlist_service.py
@@ -44,15 +44,15 @@ class PublisherBacklistService(SourceService):
 
             if not record_metadata:
                 continue
-                
+
             record = self.db_manager.session.query(Record).filter(Record.source_id == record_metadata['DRB_Record ID']).first()
 
             if not record:
                 continue
 
             try:
-                self.delete_record_digital_assets(record, record_metadata)
-                self.delete_record_data(record_metadata)
+                self.delete_record_digital_assets(record)
+                self.delete_record_data(record)
             except:
                 logger.exception(f'Failed to delete record: {record.id}')
 
@@ -60,7 +60,7 @@ class PublisherBacklistService(SourceService):
         
     def delete_record_digital_assets(self, record: Record):
         for part in record.has_part:
-            _, link, *_ = part
+            _, link, *_ = part.split('|')
             url = urlparse(link)
             bucket_name = url.hostname.split('.')[0]
             file_path = url.path.lstrip('/')
@@ -68,46 +68,55 @@ class PublisherBacklistService(SourceService):
             self.s3_manager.s3Client.delete_object(Bucket=bucket_name, Key=file_path)
 
     def delete_record_data(self, record: Record):
-        items = self.db_manager.query(Item).filter(Item.record_id == record.id).all()
-        edition_ids = set([item.edition for item in items])
+        items = self.db_manager.session.query(Item).filter(Item.record_id == record.id).all()
+        edition_ids = set([item.edition_id for item in items])
+
+        self.db_manager.session.delete(record)
 
         for item in items:
-            item.delete(synchronize_session=False)
+            self.db_manager.session.delete(item)
+
+        self.db_manager.session.commit()
 
         deleted_edition_ids = {}
         deleted_work_ids = set()
         work_ids = set()
+        work_ids_to_uuids = {}
 
         for edition_id in edition_ids:
-            edition = self.db_manager.query(Edition).join(Item).filter(Edition.id == edition_id).first()
+            edition = self.db_manager.session.query(Edition).join(Item).filter(Edition.id == edition_id).first()
 
             if len(edition.items) == 0:
-                edition.delete(synchronize_session=False)
+                self.db_manager.session.delete(edition)
                 
                 work_ids.add(edition.work_id)
                 deleted_edition_ids[edition_id] = edition.work_id
 
+        self.db_manager.session.commit()
+
         for work_id in work_ids:
-            work = self.db_manager.query(Work).join(Edition).filter(Work.id == work_id).first()
+            work = self.db_manager.session.query(Work).join(Edition).filter(Work.id == work_id).first()
+            work_ids_to_uuids[work.id] = work.uuid
 
             if len(work.editions) == 0:
-                work.delete(synchronize_session=False)
+                self.db_manager.session.delete(work)
 
                 self.es_manager.client.delete(index=os.environ['ELASTICSEARCH_INDEX'], id=work.uuid)
 
                 deleted_work_ids.add(work_id)
+
+        self.db_manager.session.commit()
         
         for edition_id, work_id in deleted_edition_ids.items():
             if work_id not in deleted_work_ids:
-                work_document = self.es_manager.client.get(index=os.environ['ELASTICSEARCH_INDEX'], id=work_id)
+                work_uuid = work_ids_to_uuids[work_id]
+                work_document = self.es_manager.client.get(index=os.environ['ELASTICSEARCH_INDEX'], id=work_uuid)
                 editions = work_document['_source'].get('editions', [])
                 
                 updated_editions = [edition for edition in editions if edition.get('id') != edition_id]
                 work_document['_source']['editions'] = updated_editions
                 
-                self.es_manager.client.index(index=os.environ['ELASTICSEARCH_INDEX'], id=work_id, body=work_document['_source'])
-
-        self.db_manager.session.commit()
+                self.es_manager.client.index(index=os.environ['ELASTICSEARCH_INDEX'], id=work_uuid, body=work_document['_source'])
 
     def get_records(
         self,
@@ -138,29 +147,24 @@ class PublisherBacklistService(SourceService):
         
     def build_filter_by_formula_parameter(self, deleted=None, full_import: bool=False, start_timestamp: datetime=None) -> str:
         if deleted:
-            deleted_filter = f"IF(%7BDRB_Deleted%7D%20%3D%20TRUE(),%20TRUE(),%20FALSE())"
-            filter_by_formula = f"&filterByFormula={deleted_filter}"
-            return filter_by_formula
+            delete_filter = urllib.parse.quote("{DRB_Deleted} = TRUE()")
+            
+            return f"&filterByFormula={delete_filter}"
         
-        is_not_deleted_filter = f"IF(%7BDRB_Deleted%7D%20!%3D%20TRUE(),%20TRUE(),%20FALSE())"
-
-        if not start_timestamp:
-            start_timestamp = datetime.now(timezone.utc).replace(tzinfo=None) - timedelta(hours=24)
-
-        if_ready_to_ingest_is_true_filter = f"%20IF(%7BDRB_Ready%20to%20ingest%7D%20%3D%20TRUE(),%20TRUE(),%20FALSE())"
+        is_not_deleted_filter = urllib.parse.quote("{DRB_Deleted} = FALSE()")
+        ready_to_ingest_filter = urllib.parse.quote("{DRB_Ready to ingest} = TRUE()")
 
         if full_import:
-            filter_by_formula = f'&filterByFormula={if_ready_to_ingest_is_true_filter}'
-            return filter_by_formula
-        else:
-            start_date_time_str = start_timestamp.strftime("%Y-%m-%d %H:%M:%S.%f")
-            start_date_time_encoded = urllib.parse.quote(start_date_time_str)
-            is_same_date_time_filter = f"IS_SAME(%7BLast%20Modified%7D,%20%22{start_date_time_encoded}%22"
-            is_after_date_time_filter = f"%20IS_AFTER(%7BLast%20Modified%7D,%20%22{start_date_time_encoded}%22"
-
-            filter_by_formula = f"&filterByFormula=AND(OR({is_same_date_time_filter}),{is_after_date_time_filter})),AND({if_ready_to_ingest_is_true_filter},{is_not_deleted_filter}))"
-
-            return filter_by_formula
+            return f'&filterByFormula=AND({ready_to_ingest_filter},{is_not_deleted_filter})'
+        
+        if not start_timestamp:
+            start_timestamp = datetime.now(timezone.utc).replace(tzinfo=None) - timedelta(hours=24)
+        
+        start_date_time_str = start_timestamp.strftime('%Y-%m-%d')
+        is_same_date_time_filter = urllib.parse.quote(f"IS_SAME({{Last Modified}}, \"{start_date_time_str}\")")
+        is_after_date_time_filter = urllib.parse.quote(f"IS_AFTER({{Last Modified}}, \"{start_date_time_str}\")")
+    
+        return f"&filterByFormula=AND(OR({is_same_date_time_filter}),{is_after_date_time_filter})),{ready_to_ingest_filter},{is_not_deleted_filter})"
         
     def get_publisher_backlist_records(self,
         deleted: bool=False,
@@ -193,7 +197,7 @@ class PublisherBacklistService(SourceService):
         # TODO: GOOGLE DRIVE API CALL TO GET PDF/EPUB FILES
         
         item_no = '1'
-        url = 'https://link-to-pdf' # TODO: get link after implementing upload to S3
+        url = 'https://drb-files-local.s3.amazonaws.com/test.pdf' # TODO: get link after implementing upload to S3
         media_tpye = 'application/pdf'
         flags = {
             'catalog': 'false',
@@ -210,7 +214,7 @@ class PublisherBacklistService(SourceService):
             item_no, url, source, media_type, flags = link.split('|')
 
             if media_type == 'application/pdf':
-                manifest_path = f'manifests/publisher_backlist/{source}/{record.id}.json'
+                manifest_path = f'manifests/publisher_backlist/{source}/{record.source_id}.json'
                 manifest_url = f'https://{self.file_bucket}.s3.amazonaws.com/{manifest_path}'
 
                 manifest_json = self.generate_manifest(record, url, manifest_url)
