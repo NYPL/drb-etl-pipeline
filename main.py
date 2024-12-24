d90ced56632833452d6323d682869c289e7b755f
kyle <kylejacksonvillegas@nypl.org>
2024-12-24 12:57:16 -0500

    removing traceback

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import newrelic.agent
 
 import argparse
 import inspect
-import traceback
 
 from load_env import load_env_file
 from logger import create_log
@@ -44,7 +43,6 @@ def main(args):
         )
     except:
         logger.exception(f'Failed to initialize process {process} in {environment}')
-        logger.error(traceback.format_exc())
         return
 
     if process in (
