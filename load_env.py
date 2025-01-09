905b209bb91eed0b569a46030fd52539919a290e
kyle <kylejacksonvillegas@nypl.org>
2025-01-09 15:35:53 -0500

    SFR-2472: Load secrets when loading env

--- load_env.py
+++ load_env.py
@@ -2,6 +2,35 @@ import os
 from typing import Optional
 import yaml
 
+from services.ssm_service import SSMService
+
+
+ENV_VAR_TO_SSM_NAME = {
+    'CONTENT_CAFE_USER': 'contentcafe/user',
+    'CONTENT_CAFE_PSWD': 'contentcafe/pswd',
+    'ELASTICSEARCH_PSWD': 'elasticsearch/pswd',
+    'ELASTICSEARCH_USER': 'elasticsearch/user',
+    'GITHUB_API_KEY': 'github-key',
+    'GOOGLE_BOOKS_KEY': 'google-books/api-key',
+    'HATHI_API_KEY': 'hathitrust/api-key',
+    'HATHI_API_SECRET': 'hathitrust/api-secret',
+    'NEW_RELIC_LICENSE_KEY': 'newrelic/key',
+    'NYPL_API_CLIENT_ID': 'nypl-api/client-id',
+    'NYPL_API_CLIENT_PUBLIC_KEY': 'nypl-api/public-key',
+    'NYPL_API_CLIENT_SECRET': 'nypl-api/client-secret',
+    'NYPL_BIB_PSWD': 'postgres/nypl-pswd',
+    'NYPL_BIB_USER': 'postgres/nypl-user',
+    'OCLC_API_KEY': 'oclc-key',
+    'OCLC_METADATA_ID': 'oclc-metadata-clientid',
+    'OCLC_METADATA_SECRET': 'oclc-metadata-secret',
+    'OCLC_CLIENT_ID': 'oclc-search-clientid',
+    'OCLC_CLIENT_SECRET': 'oclc-search-secret',
+    'POSTGRES_PSWD': 'postgres/pswd',
+    'POSTGRES_USER': 'postgres/user',
+    'RABBIT_PSWD': 'rabbit-pswd',
+    'RABBIT_USER': 'rabbit-user',
+}
+
 
 def load_env_file(run_type: str, file_string: Optional[str]=None) -> None:
     """Loads configuration details from a specific yaml file.
@@ -37,3 +66,12 @@ def load_env_file(run_type: str, file_string: Optional[str]=None) -> None:
     if env_dict:
         for key, value in env_dict.items():
             os.environ[key] = value
+
+    load_secrets()
+            
+def load_secrets():
+    ssm_service = SSMService()
+
+    for env_var, param_name in ENV_VAR_TO_SSM_NAME.items():
+        if os.environ.get(env_var, None) is None:
+            os.environ[env_var] = ssm_service.get_parameter(param_name)
