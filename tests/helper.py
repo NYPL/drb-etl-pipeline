8224042b6c2d0baf59e91b0ae13b1700bd7e3774
kyle <kylejacksonvillegas@nypl.org>
2025-01-13 14:57:01 -0500

    SFr-2478: Setting github api url as a constant

--- tests/helper.py
+++ tests/helper.py
@@ -42,7 +42,6 @@ class TestHelpers:
         'NYPL_API_CLIENT_SECRET': 'test_api_secret',
         'NYPL_API_CLIENT_TOKEN_URL': 'test_api_token_url',
         'GITHUB_API_KEY': 'test_github_key',
-        'GITHUB_API_ROOT': 'test_github_url',
         'BARDO_CCE_API': 'test_cce_url',
         'MUSE_MARC_URL': 'test_muse_url',
         'MUSE_CSV_URL': 'test_muse_csv',
