a42569d232d96720da23c178d47ac95ebdfec9c8
Kyle Villegas <86266231+kylevillegas93@users.noreply.github.com>
2025-01-16 10:51:08 -0500

    NO-REF: Setup manifest testing for MET (#526)

--- tests/functional/processes/ingest/test_met_process.py
+++ tests/functional/processes/ingest/test_met_process.py
@@ -1,10 +1,17 @@
-from processes import LOCProcess
+from processes import METProcess
 from .assert_ingested_records import assert_ingested_records
 
 
 def test_met_process():
-    met_process = LOCProcess('complete', None, None, None, 5, None)
+    met_process = METProcess('complete', None, None, None, 5, None)
 
     met_process.runProcess()
 
-    assert_ingested_records(source_name='loc')
+    records = assert_ingested_records(source_name='met')
+
+    # assert that for each record there exists a PDF manifest in S3
+    
+    # run the S3 process
+
+    # assert that for each record, we have saved the PDF in S3
+
