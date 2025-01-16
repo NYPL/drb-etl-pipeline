3e8b11c8862dc097773c4ab87d5e5823ca7d5dbb
kyle <kylejacksonvillegas@nypl.org>
2025-01-16 10:26:29 -0500

    NO-REF: Setup manifest testing for MET

--- tests/functional/processes/ingest/assert_ingested_records.py
+++ tests/functional/processes/ingest/assert_ingested_records.py
@@ -2,9 +2,8 @@ from datetime import datetime, timezone, timedelta
 
 from managers import DBManager
 from model import Record
-from processes.core import CoreProcess
 
-def assert_ingested_records(source_name: str):
+def assert_ingested_records(source_name: str) -> list[Record]:
     db_manager = DBManager()
     db_manager.generateEngine()
     db_manager.createSession()
@@ -17,3 +16,5 @@ def assert_ingested_records(source_name: str):
     )
 
     assert len(records) > 1
+
+    return records
