c253c06d83a337c6fd8b5c8d233ff5a78f2a4017
ayan <100955969+ayan1229@users.noreply.github.com>
2025-01-24 14:53:56 -0500

    updating changes per review

--- processes/local_development/seed_test_data.py
+++ processes/local_development/seed_test_data.py
@@ -7,6 +7,7 @@ from managers import DBManager
 
 logger = create_log(__name__)
 
+TEST_SOURCE = 'test_source'
 
 class SeedTestDataProcess():
     def __init__(self, *args):
@@ -15,16 +16,14 @@ class SeedTestDataProcess():
         self.test_data = {
             'title': 'test data 1',
             'uuid' : uuid4(),
-            'date_created' : datetime.now(timezone.utc).replace(tzinfo=None),
-            'date_modified' :datetime.now(timezone.utc).replace(tzinfo=None),
             'frbr_status': 'complete',
             'cluster_status': False,
-            "source": 'test data source',
+            "source": TEST_SOURCE,
             'authors': ['Ayan||true'],
             'languages': ['Serbian'],
             'dates': ['1907-|publication_date'],
             'publisher': ['Project Gutenberg Literary Archive Foundation||'],
-            'identifiers': ['4064148285|owi','161468|oclc','161468|oclc','972173562|oclc','1467194159|oclc'],
+            'identifiers': [],
             'source_id': '4064148285|test',
             'contributors': ['Metropolitan Museum of Art (New York, N.Y.)|||contributor','Metropolitan Museum of Art (New York, N.Y.)|||repository','Thomas J. Watson Library|||provider'],
             'extent': ('11, 164 p. ;'),
@@ -32,7 +31,7 @@ class SeedTestDataProcess():
             'abstract': ['test abstract 1', 'test abstract 2'],
             'subjects': ['test subjects 1||'],
             'rights': ('hathitrust|public_domain|expiration of copyright term for non-US work with corporate author|Public Domain|2021-10-02 05:25:13'),
-            'has_part': ['1|example.com/1.pdf|met|text/html|{"catalog": false, "download": false, "reader": false, "embed": true}']
+            'has_part': ['1|example.com/1.pdf|{TEST_SOURCE}}|text/html|{"catalog": false, "download": false, "reader": false, "embed": true}']
         }
 
     def runProcess(self):
@@ -47,7 +46,17 @@ class SeedTestDataProcess():
             self.db_manager.close_connection()
 
     def save_test_record(self):
-        test_record = Record(**self.test_data)
+        existing_record = self.db_manager.session.query(Record).filter_by(source_id=self.test_data['source_id']).first()
 
-        self.db_manager.session.add(test_record)
+        if existing_record:
+            for key, value in self.test_data.items():
+                if key != 'uuid' and hasattr(existing_record, key):
+                    setattr(existing_record, key, value)
+        
+            existing_record.date_modified = datetime.now(timezone.utc).replace(tzinfo=None)
+
+        else:
+            test_record = Record(**self.test_data)
+            self.db_manager.session.add(test_record)
+            
         self.db_manager.session.commit()
