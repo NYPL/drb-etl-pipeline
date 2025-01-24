7b9242644f38a7165f0b52c4e9e3c46312936440
Shejanul Ayan Islam <100955969+ayan1229@users.noreply.github.com>
2025-01-24 15:06:40 -0500

    update frbr_status to complete (#539)
    * update frbr_status to complete
    
    * updating changes per review
    
    * fixing has_part
    
    ---------
    
    Co-authored-by: kyle <kylejacksonvillegas@nypl.org>

--- processes/local_development/seed_test_data.py
+++ processes/local_development/seed_test_data.py
@@ -1,30 +1,31 @@
-from datetime import datetime
-from uuid import uuid4
 from datetime import datetime, timezone
+import json
+from uuid import uuid4
+
 from model import Record
 from logger import create_log
 from managers import DBManager
 
 logger = create_log(__name__)
 
+TEST_SOURCE = 'test_source'
 
 class SeedTestDataProcess():
     def __init__(self, *args):
-
         self.db_manager = DBManager()
+
+        flags = { 'catalog': False, 'download': False, 'reader': False, 'embed': True }
         self.test_data = {
             'title': 'test data 1',
             'uuid' : uuid4(),
-            'date_created' : datetime.now(timezone.utc).replace(tzinfo=None),
-            'date_modified' :datetime.now(timezone.utc).replace(tzinfo=None),
-            'frbr_status': 'to_do',
+            'frbr_status': 'complete',
             'cluster_status': False,
-            "source": 'test data source',
+            "source": TEST_SOURCE,
             'authors': ['Ayan||true'],
             'languages': ['Serbian'],
             'dates': ['1907-|publication_date'],
             'publisher': ['Project Gutenberg Literary Archive Foundation||'],
-            'identifiers': ['4064148285|owi','161468|oclc','161468|oclc','972173562|oclc','1467194159|oclc'],
+            'identifiers': [],
             'source_id': '4064148285|test',
             'contributors': ['Metropolitan Museum of Art (New York, N.Y.)|||contributor','Metropolitan Museum of Art (New York, N.Y.)|||repository','Thomas J. Watson Library|||provider'],
             'extent': ('11, 164 p. ;'),
@@ -32,7 +33,7 @@ class SeedTestDataProcess():
             'abstract': ['test abstract 1', 'test abstract 2'],
             'subjects': ['test subjects 1||'],
             'rights': ('hathitrust|public_domain|expiration of copyright term for non-US work with corporate author|Public Domain|2021-10-02 05:25:13'),
-            'has_part': ['1|example.com/1.pdf|met|text/html|{"catalog": false, "download": false, "reader": false, "embed": true}']
+            'has_part': [f'1|example.com/1.pdf|{TEST_SOURCE}|text/html|{json.dumps(flags)}']
         }
 
     def runProcess(self):
@@ -53,9 +54,10 @@ class SeedTestDataProcess():
             for key, value in self.test_data.items():
                 if key != 'uuid' and hasattr(existing_record, key):
                     setattr(existing_record, key, value)
+        
             existing_record.date_modified = datetime.now(timezone.utc).replace(tzinfo=None)
         else:
             test_record = Record(**self.test_data)
             self.db_manager.session.add(test_record)
-
+        
         self.db_manager.session.commit()
