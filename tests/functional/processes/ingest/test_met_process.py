d00ca2a27075b32777e4815b758cab2e11746006
ayan <100955969+ayan1229@users.noreply.github.com>
2025-01-27 10:24:49 -0500

    first commit for kyle to review

--- tests/functional/processes/ingest/test_met_process.py
+++ tests/functional/processes/ingest/test_met_process.py
@@ -1,17 +1,26 @@
 from processes import METProcess
 from .assert_ingested_records import assert_ingested_records
+from managers.s3 import S3Manager, S3Error
 
 
 def test_met_process():
+
     met_process = METProcess('complete', None, None, None, 5, None)
 
     met_process.runProcess()
-
     records = assert_ingested_records(source_name='met')
 
-    # assert that for each record there exists a PDF manifest in S3
     
-    # run the S3 process
+    s3_manager = S3Manager()
+    s3_manager.createS3Client() 
 
-    # assert that for each record, we have saved the PDF in S3
+    s3_bucket_name = 'drb-files-local'
+    for record in records:
+        pdf_key = f"manifests/{record.id}.pdf"
 
+        try:
+            s3_manager.getObjectFromBucket(pdf_key, s3_bucket_name)
+        except S3Error as e:
+            assert False, f"PDF manifest for record {record.id} does not exist in S3: {e.message}"
+        except Exception as e:
+            assert False, f"An error occurred while checking for the PDF manifest: {str(e)}"
