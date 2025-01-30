c231b8ef00eefca3fbcd914b53ce362d8c5ca99e
kyle <kylejacksonvillegas@nypl.org>
2025-01-30 10:25:29 -0500

    changing whitespace

--- tests/conftest.py
+++ tests/conftest.py
@@ -40,7 +40,7 @@ def db_manager():
         db_manager.session.execute(text('SELECT 1')) # Test the db connection
 
         yield db_manager
-    
+        
         db_manager.close_connection()
     except:
         yield None
