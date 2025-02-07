72d21eb6b00f128fb922db29df635b7b7bdc6554
Dmitri <you@example.com>
2025-02-07 14:44:23 -0500

    Deleted CLACSOService import

--- services/__init__.py
+++ services/__init__.py
@@ -1,5 +1,4 @@
 from .sources.nypl_bib_service import NYPLBibService
 from .sources.publisher_backlist_service import PublisherBacklistService
 from .google_drive_service import GoogleDriveService
-from .sources.clacso_service import CLACSOService
 from .sources.dspace_service import DSpaceService
