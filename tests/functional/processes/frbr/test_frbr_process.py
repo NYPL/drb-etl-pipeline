f3c2507dd351ecdd9c63bcbd9c96cf7d5e4cc77a
kyle <kylejacksonvillegas@nypl.org>
2025-02-07 14:12:19 -0500

    NO-REF: Complete FRBR functional test

--- tests/functional/processes/frbr/test_frbr_process.py
+++ tests/functional/processes/frbr/test_frbr_process.py
@@ -16,3 +16,8 @@ def test_frbr_process(db_manager, unfrbrized_record_uuid):
     frbrized_record = db_manager.session.query(Record).filter(Record.uuid == unfrbrized_record_uuid).first()
     
     assert frbrized_record.frbr_status == 'complete'
+
+    ## TODO:
+    # 1. assert there is at least 1 record where the source is oclcClassify updated in the last minute
+    # 2. run the catalog process
+    # 3. assert there is at least 1 record where the source is oclcCatalog updated in the last minute
