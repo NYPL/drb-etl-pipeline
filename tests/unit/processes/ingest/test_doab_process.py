bba866cf3a936acd7fe2ede8af04aa3be7fe08fb
Jackie Quach <37321861+jackiequach@users.noreply.github.com>
2025-02-10 16:55:24 -0500

    NO-REF: Update DOABProcess to use RecordBuffer (#563)
    * update DOABProcess to use RecordBuffer
    
    * re-add close_connection call

--- tests/unit/processes/ingest/test_doab_process.py
+++ tests/unit/processes/ingest/test_doab_process.py
@@ -17,6 +17,11 @@ class TestDOABProcess:
     def test_instance(self, mocker):
         class TestDOAB(DOABProcess):
             def __init__(self):
+                self.process = 'test_process'
+                self.ingest_period = None
+                self.single_record = 0
+                self.db_manager = mocker.MagicMock()
+                self.record_buffer = mocker.MagicMock()
                 self.s3_manager = mocker.MagicMock(s3Client=mocker.MagicMock())
                 self.s3_bucket = 'test_aws_bucket'
                 self.file_queue = 'test_file_queue'
@@ -24,7 +29,6 @@ class TestDOABProcess:
                 self.rabbitmq_manager = mocker.MagicMock()
                 self.offset = 0
                 self.limit = 10000
-                self.ingestPeriod = None
                 self.records = []
                 self.dspace_service = mocker.MagicMock()
 
@@ -41,20 +45,8 @@ class TestDOABProcess:
     @pytest.fixture
     def mocks(self, mocker):
         return {
-            'generate_engine': mocker.patch.object(DOABProcess, 'generateEngine'),
-            'create_session': mocker.patch.object(DOABProcess, 'createSession'),
-            'save': mocker.patch.object(DOABProcess, 'saveRecords'),
-            'commit': mocker.patch.object(DOABProcess, 'commitChanges'),
-            'close': mocker.patch.object(DOABProcess, 'close_connection'),
             'manage_links': mocker.patch.object(DOABProcess, 'manage_links'),
         }
-
-    def assert_common_expectations(self, mocks):
-        mocks['generate_engine'].assert_called_once()
-        mocks['create_session'].assert_called_once()
-        mocks['save'].assert_called_once()
-        mocks['commit'].assert_called_once()
-        mocks['close'].assert_called_once()
 
     def test_runProcess_daily(self, test_instance: DOABProcess, record_mappings, mocks):
         test_instance.dspace_service.get_records.return_value = record_mappings
@@ -64,7 +56,6 @@ class TestDOABProcess:
 
         test_instance.dspace_service.get_records.assert_called_once_with(offset=0, limit=10000)
         assert mocks['manage_links'].call_count == len(record_mappings)
-        self.assert_common_expectations(mocks)
 
     def test_runProcess_complete(self, test_instance: DOABProcess, record_mappings, mocks):
         test_instance.dspace_service.get_records.return_value = record_mappings
@@ -74,7 +65,6 @@ class TestDOABProcess:
 
         test_instance.dspace_service.get_records.assert_called_once_with(full_import=True, offset=0, limit=10000)
         assert mocks['manage_links'].call_count == len(record_mappings)
-        self.assert_common_expectations(mocks)
 
     def test_runProcess_custom(self, test_instance: DOABProcess, record_mappings, mocks):
         test_instance.dspace_service.get_records.return_value = record_mappings
@@ -84,18 +74,16 @@ class TestDOABProcess:
 
         test_instance.dspace_service.get_records.assert_called_once_with(start_timestamp=None, offset=0, limit=10000)
         assert mocks['manage_links'].call_count == len(record_mappings)
-        self.assert_common_expectations(mocks)
 
     def test_runProcess_single(self, test_instance: DOABProcess, single_record_mapping, mocks):
         test_instance.dspace_service.get_single_record.return_value = single_record_mapping
         
         test_instance.process = 'single'
-        test_instance.singleRecord = 1
+        test_instance.single_record = 1
         test_instance.runProcess()
 
         test_instance.dspace_service.get_single_record.assert_called_once_with(record_id=1, source_identifier='oai:directory.doabooks.org')
         assert mocks['manage_links'].call_count == 1
-        self.assert_common_expectations(mocks)
 
 
     def test_manage_links_success(self, test_instance: DOABProcess, mocker):
@@ -108,8 +96,6 @@ class TestDOABProcess:
         mock_link_manager = mocker.patch('processes.ingest.doab.DOABLinkManager')
         mock_link_manager.return_value = mock_manager
 
-        process_mocks = mocker.patch.multiple(DOABProcess, addDCDWToUpdateList=mocker.DEFAULT)
-
         test_instance.manage_links(mock_record)
 
         mock_manager.parseLinks.assert_called_once()
@@ -119,4 +105,3 @@ class TestDOABProcess:
             test_instance.file_route, 
             get_file_message('epubURI', 'epubPath')
         )
-        process_mocks['addDCDWToUpdateList'].assert_called_once()
