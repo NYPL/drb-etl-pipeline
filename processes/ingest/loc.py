8ad27da2f4b3e02a8f6061f596879bb286ac4eb1
kyle <kylejacksonvillegas@nypl.org>
2025-02-27 13:27:23 -0500

    removing loc changes

--- processes/ingest/loc.py
+++ processes/ingest/loc.py
@@ -280,10 +280,7 @@ class LOCProcess(CoreProcess):
     
     @staticmethod
     def fetchPageJSON(url):
-        headers = {
-            'Accept': 'application/json',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-        }
+        headers = {'Accept': 'application/json'}
         elemResponse = requests.get(url, headers=headers)
         elemResponse.raise_for_status()
         return elemResponse
