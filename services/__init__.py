3467f8ad15393906fcbabce7f0a3bb94ff09a744
Kyle Villegas <86266231+kylevillegas93@users.noreply.github.com>
2025-02-28 09:39:39 -0500

    SFR-2525: Refactor Gutenberg ingest to use source service (#594)

--- services/__init__.py
+++ services/__init__.py
@@ -2,3 +2,4 @@ from .sources.nypl_bib_service import NYPLBibService
 from .sources.publisher_backlist_service import PublisherBacklistService
 from .google_drive_service import GoogleDriveService
 from .sources.dspace_service import DSpaceService
+from .sources.gutenberg_service import GutenbergService
