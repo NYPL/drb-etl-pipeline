8d43125da1f1638cb33b13d7762727dbdd0abe80
kyle <kylejacksonvillegas@nypl.org>
2025-03-04 10:37:06 -0500

    fixing typo

--- tests/integration/services/sources/test_met_service.py
+++ tests/integration/services/sources/test_met_service.py
@@ -3,9 +3,9 @@ from services import METService
 
 
 def test_get_records():
-    gutenberg_service = METService()
+    met_service = METService()
 
-    records = gutenberg_service.get_records(limit=100)
+    records = met_service.get_records(limit=100)
 
     for record in records:
         assert record is not None
