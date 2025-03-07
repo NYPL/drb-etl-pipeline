1ee685c52e16f8c642635a283565d84f7b096dee
kyle <kylejacksonvillegas@nypl.org>
2025-03-07 13:20:43 -0500

    NO-REF: Adding functin to parse process args

--- processes/utils.py
+++ processes/utils.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from datetime import datetime, timedelta, timezone
 from typing import Optional
 
@@ -12,3 +13,26 @@ def get_start_datetime(process_type: Optional[str]=None, ingest_period: Optional
         return datetime.now(timezone.utc).replace(tzinfo=None) - timedelta(days=7)
     
     return None
+
+
+@dataclass
+class ProcessParams:
+    process_type: str = 'daily'
+    custom_file: Optional[str] = None
+    ingest_period: Optional[str] = None
+    record_id: Optional[str] = None
+    limit: Optional[int] = None
+    offset: int = 0
+    source: Optional[str] = None
+
+
+def parse_process_args(*args) -> ProcessParams:
+    return ProcessParams(
+        process_type=args[0] if len(args) >= 1 else 'daily',
+        custom_file=args[1] if len(args) >= 2 else None,
+        ingest_period=args[2] if len(args) >= 3 else None,
+        record_id=args[3] if len(args) >= 4 else None,
+        limit=int(args[4]) if len(args) >= 5 else None,
+        offset=int(args[5]) if len(args) >= 6 else 0,
+        source=args[6] if len(args) >= 7 else None,
+    )
