a5949d932bc632567a267aeaffc937619ee16a13
kyle <kylejacksonvillegas@nypl.org>
2025-03-12 16:50:49 -0400

    updating dspace integration test

--- tests/functional/processes/ingest/test_clacso_process.py
+++ tests/functional/processes/ingest/test_clacso_process.py
@@ -1,10 +1,10 @@
-from processes import CLACSOProcess
+from processes import DOABProcess
 from .assert_ingested_records import assert_ingested_records
 from .assert_uploaded_manifests import assert_uploaded_manifests
 
 
 def test_clacso_process():
-    clacso_process = CLACSOProcess('complete', None, None, None, 5, None)
+    clacso_process = DOABProcess('complete', None, None, None, 5, None)
 
     clacso_process.runProcess()
 
