113c531ff77e748852ca266eb6efd16d9675b351
kyle <kylejacksonvillegas@nypl.org>
2025-03-12 17:00:24 -0400

    oops

--- tests/functional/processes/ingest/test_clacso_process.py
+++ tests/functional/processes/ingest/test_clacso_process.py
@@ -1,12 +1,12 @@
-from processes import DOABProcess
+from processes import CLACSOProcess
 from .assert_ingested_records import assert_ingested_records
 from .assert_uploaded_manifests import assert_uploaded_manifests
 
 
 def test_clacso_process():
-    clacso_process = DOABProcess('complete', None, None, None, 5, None)
+    clacso_process = CLACSOProcess('complete', None, None, None, 5, None)
 
-    clacso_process.runProcess()
+    number_of_records_ingested = clacso_process.runProcess()
 
-    records = assert_ingested_records(source_name='clacso')
+    records = assert_ingested_records(source_name='clacso', expected_number_of_records=number_of_records_ingested)
     assert_uploaded_manifests(records)
