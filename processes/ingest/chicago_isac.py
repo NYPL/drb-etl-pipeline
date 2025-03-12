fb9df91f5d58b5d80f55cf29c77dabe62c33564a
kyle <kylejacksonvillegas@nypl.org>
2025-03-12 13:00:05 -0400

    changing to composition

--- processes/ingest/chicago_isac.py
+++ processes/ingest/chicago_isac.py
@@ -4,13 +4,12 @@ from ..record_ingestor import RecordIngestor
 from .. import utils
 
 
-class ChicagoISACProcess(RecordIngestor):
+class ChicagoISACProcess():
 
     def __init__(self, *args):
         self.chicago_isac_service = ChicagoISACService()
-        super().__init__(self.chicago_isac_service, Source.CHICACO_ISAC.value)
-
         self.params = utils.parse_process_args(*args)
+        self.record_ingestor = RecordIngestor(source_service=self.chicago_isac_service, source=Source.CHICACO_ISAC.value)
 
     def runProcess(self):    
-        return self.ingest(params=self.params)
+        return self.record_ingestor.ingest(params=self.params)
