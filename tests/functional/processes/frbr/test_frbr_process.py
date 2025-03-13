efd3054c9fd5373b26eaa6254f131a55cc53ed83
ayan <100955969+ayan1229@users.noreply.github.com>
2025-03-13 09:18:37 -0400

    adding a print for testing

--- tests/functional/processes/frbr/test_frbr_process.py
+++ tests/functional/processes/frbr/test_frbr_process.py
@@ -41,5 +41,5 @@ def test_frbr_process(db_manager, unfrbrized_record_uuid):
             )
             .all()
     )
-    
+    print(catalog_records)
     assert len(catalog_records) == len(oclc_identifiers)
