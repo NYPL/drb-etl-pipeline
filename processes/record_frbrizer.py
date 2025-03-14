50c7775c5970894c3696fecf60c4084ea9b5f48d
kyle <kylejacksonvillegas@nypl.org>
2025-03-14 12:30:19 -0400

    addressing PR comments

--- processes/record_frbrizer.py
+++ processes/record_frbrizer.py
@@ -1,4 +1,3 @@
-from collections import defaultdict
 from lxml import etree
 import re
 
@@ -26,8 +25,7 @@ class RecordFRBRizer:
         self.record_buffer = RecordBuffer(db_manager=self.db_manager)
 
     def frbrize_record(self, record: Record) -> Record:
-        owi_to_oclc_numbers = self._add_works_for_record(record=record)
-        self._add_edition_records(owi_to_oclc_numbers=owi_to_oclc_numbers)
+        self._add_works_for_record(record=record)
 
         self.record_buffer.flush()
 
@@ -40,81 +38,64 @@ class RecordFRBRizer:
 
         return record
 
-    def _add_works_for_record(self, record: Record) -> defaultdict:
-        queryable_ids = self._get_queryable_identifiers(record.identifiers)
+    def _add_works_for_record(self, record: Record):
         author = record.authors[0].split('|')[0] if record.authors else None
         title = record.title
 
-        owi_to_oclc_numbers = defaultdict(set)
-
-        for id, id_type in queryable_ids:
+        for id, id_type in self._get_queryable_identifiers(record.identifiers):
             if self.redis_manager.checkSetRedis('classify', id, id_type):
                 continue
 
             search_query = self.oclc_catalog_manager.generate_search_query(identifier=id, identifier_type=id_type)
-            owi_to_oclc_numbers.update(self._add_works(self.oclc_catalog_manager.query_bibs(query=search_query)))
+            self._add_works(self.oclc_catalog_manager.query_bibs(query=search_query))
 
-        if len(owi_to_oclc_numbers) == 0 and author and title:
+        if self.record_buffer.ingest_count == 0 and len(self.record_buffer.records) == 0 and author and title:
             search_query = self.oclc_catalog_manager.generate_search_query(author=author, title=title)
-            owi_to_oclc_numbers.update(self._add_works(self.oclc_catalog_manager.query_bibs(query=search_query)))
+            self._add_works(self.oclc_catalog_manager.query_bibs(query=search_query))
 
-        return owi_to_oclc_numbers
-
-    def _add_works(self, oclc_bibs: list) -> set:
-        owi_to_oclc_numbers = defaultdict(set)
-
+    def _add_works(self, oclc_bibs: list):
         for oclc_bib in oclc_bibs:
-            oclc_number = oclc_bib.get('identifier', {}).get('oclcNumber')
-            owi_number = oclc_bib.get('work', {}).get('id')
-
-            if not oclc_number or not owi_number:
-                logger.warning(f'Unable to get identifiers for bib: {oclc_bib}')
-                continue
-
-            if self.redis_manager.checkSetRedis('classifyWork', owi_number, 'owi'):
-                continue
-
-            related_oclc_numbers = self.oclc_catalog_manager.get_related_oclc_numbers(oclc_number=oclc_number)
-
-            oclc_bib_mapping = OCLCBibMapping(
-                oclc_bib=oclc_bib,
-                related_oclc_numbers=list(set(related_oclc_numbers))
-            )
-
-            self.record_buffer.add(record=oclc_bib_mapping.record)
-            owi_to_oclc_numbers[owi_number] = set(related_oclc_numbers)
-
-        return owi_to_oclc_numbers
-
-    def _add_edition_records(self, owi_to_oclc_numbers: defaultdict[set]):
-        for owi_number, oclc_numbers in owi_to_oclc_numbers.items():
-            cached_oclc_numbers = self.redis_manager.multiCheckSetRedis('catalog', list(oclc_numbers), 'oclc')
-
-            for oclc_number, uncached in cached_oclc_numbers:
+            owi_number, related_oclc_numbers = self._add_work(oclc_bib) 
+            
+            for oclc_number, uncached in self.redis_manager.multiCheckSetRedis('catalog', related_oclc_numbers, 'oclc'):
                 if not uncached:
                     continue
-                
-                self._add_edition_record(oclc_number=oclc_number, owi_number=owi_number)
 
-    def _add_edition_record(self, oclc_number: int, owi_number: str):
-        catalog_record = self.oclc_catalog_manager.query_catalog(oclc_number)
+                self._add_edition(owi_number, oclc_number)
 
+    def _add_work(self, oclc_bib: dict) -> tuple:
+        oclc_number = oclc_bib.get('identifier', {}).get('oclcNumber')
+        owi_number = oclc_bib.get('work', {}).get('id')
+        related_oclc_numbers = []
+
+        if not oclc_number or not owi_number:
+            logger.warning(f'Unable to get identifiers for bib: {oclc_bib}')
+            return (owi_number, related_oclc_numbers)
+
+        if self.redis_manager.checkSetRedis('classifyWork', owi_number, 'owi'):
+            return (owi_number, related_oclc_numbers)
+
+        related_oclc_numbers = self.oclc_catalog_manager.get_related_oclc_numbers(oclc_number=oclc_number)
+
+        oclc_bib_mapping = OCLCBibMapping(oclc_bib=oclc_bib,related_oclc_numbers=list(set(related_oclc_numbers)))
+        self.record_buffer.add(record=oclc_bib_mapping.record)
+
+        return (owi_number, related_oclc_numbers)
+
+    def _add_edition(self, owi_number: int, oclc_number: str):
         try:
+            catalog_record = self.oclc_catalog_manager.query_catalog(oclc_number)
+
             parsed_marc_xml = etree.fromstring(catalog_record.encode('utf-8'))
-        except Exception:
-            logger.exception(f'Unable to parse OCLC catalog MARC XML for OCLC number: {oclc_number}')
-            return
-
-        catalog_record_mapping = CatalogMapping(parsed_marc_xml, { 'oclc': 'http://www.loc.gov/MARC21/slim' }, {})
-
-        try:
+            
+            catalog_record_mapping = CatalogMapping(parsed_marc_xml, { 'oclc': 'http://www.loc.gov/MARC21/slim' }, {})
             catalog_record_mapping.applyMapping()
-            catalog_record_mapping.record.identifiers.append('{}|owi'.format(owi_number))
+            catalog_record_mapping.record.identifiers.append(f'{owi_number}|owi')
             
             self.record_buffer.add(catalog_record_mapping.record)
         except Exception:
-            logger.exception(f'Unable to map OCLC catalog record for OCLC number: {oclc_number}')
+            logger.exception(f'Unable to add edition with OCLC number: {oclc_number}')
             return
 
     def _get_queryable_identifiers(self, identifiers) -> set:
-        return set([(id.split('|')[0], id.split('|')[1]) for id in identifiers if re.search(r'\|(?:isbn|issn|oclc)$', id) != None])
+        return { tuple(id.split('|', 1)) for id in identifiers if re.search(r'\|(?:isbn|issn|oclc)$', id) != None }
