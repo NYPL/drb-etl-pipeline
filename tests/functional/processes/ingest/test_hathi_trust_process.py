18fb3af76c5c58fc84209555ca1ab74e44049528
Kyle Villegas <86266231+kylevillegas93@users.noreply.github.com>
2025-03-14 13:58:41 -0400

    NO-REF: Migrating Hathi to use source service (#621)

--- tests/functional/processes/ingest/test_hathi_trust_process.py
+++ tests/functional/processes/ingest/test_hathi_trust_process.py
@@ -3,7 +3,7 @@ from .assert_ingested_records import assert_ingested_records
 
 
 def test_hathi_trust_process():
-    hathi_trust_process = HathiTrustProcess('daily', None, None, None, 5, None)
+    hathi_trust_process = HathiTrustProcess('weekly', None, None, None, 5, None)
 
     number_of_records_ingested = hathi_trust_process.runProcess()
 
