ce99d0f60c999d35cecee9a739d5737e362904b8
Kyle Villegas <86266231+kylevillegas93@users.noreply.github.com>
2025-03-17 10:30:40 -0400

    SFR-2550: Implementing Record Fulfiller step (#622)

--- processes/__init__.py
+++ processes/__init__.py
@@ -16,8 +16,8 @@ from .util.db_maintenance import DatabaseMaintenanceProcess
 from .util.db_migration import MigrationProcess
 from .ingest.chicago_isac import ChicagoISACProcess
 from .ingest.loc import LOCProcess
-from .file.fulfill_url_manifest import FulfillURLManifestProcess
 from .ingest.publisher_backlist import PublisherBacklistProcess
 from .ingest.clacso import CLACSOProcess
 from .record_ingestor import RecordIngestor
+from .link_fulfiller import LinkFulfiller
 from .record_frbrizer import RecordFRBRizer
