a5f6b5ba0f4af8861f7ae6d14334e7e4e5d83c9c
kyle <kylejacksonvillegas@nypl.org>
2025-03-27 10:32:46 -0400

    simplifying

--- processes/ingest/gutenberg_v2.py
+++ processes/ingest/gutenberg_v2.py
@@ -36,14 +36,7 @@ class GutenbergV2Process():
         for record_mapping in records:
             self.store_epubs(record_mapping)
 
-            try:
-                self.add_cover(record_mapping)
-            except Exception:
-                logger.warning(f'Unable to store cover for {record_mapping.record.source_id}')
-
     def store_epubs(self, gutenberg_record: GutenbergMapping):
-        epub_parts = []
-
         for part in gutenberg_record.record.parts:
             epub_id_parts = re.search(r'\/([0-9]+).epub.([a-z]+)$', part.url)
             gutenberg_id = epub_id_parts.group(1)
@@ -51,65 +44,8 @@ class GutenbergV2Process():
 
             if json.loads(part.flags).get('download', False) is True:
                 epub_path = f'epubs/{part.source}/{gutenberg_id}_{gutenberg_type}.epub'
-                epub_url = get_stored_file_url(self.file_bucket, epub_path)
 
-                epub_parts.append(str(Part(
-                    index=part.index,
-                    source=part.source,
-                    url=epub_url,
-                    file_type=part.file_type,
-                    flags=part.flags
-                )))
-
-                self.record_file_saver.store_file(file_url=part.url, file_path=epub_path)
-            else:
-                container_path = f'epubs/{part.source}/{gutenberg_id}_{gutenberg_type}/META-INF/container.xml'
-                manifest_path = f'epubs/{part.source}/{gutenberg_id}_{gutenberg_type}/manifest.json'
-
-                epub_parts.append(str(Part(
-                    index=part.index,
-                    source=part.source,
-                    url=get_stored_file_url(self.file_bucket, container_path),
-                    file_type='application/epub+xml',
-                    flags=part.flags
-                )))
-
-                epub_parts.append(str(Part(
-                    index=part.index,
-                    source=part.source,
-                    url=get_stored_file_url(self.file_bucket, manifest_path),
-                    file_type='application/epub+xml',
-                    flags=part.flags
-                )))
-
-        gutenberg_record.record.has_part = epub_parts
-
-    def add_cover(self, gutenberg_record: GutenbergMapping):
-        yaml_file = gutenberg_record.yaml_file
-
-        if yaml_file is None:
-            return
-
-        for cover_data in yaml_file.get('covers', []):
-            if cover_data.get('cover_type') == 'generated': 
-                continue
-
-            mime_type, _ = mimetypes.guess_type(cover_data.get('image_path'))
-            gutenberg_id = yaml_file.get('identifiers', {}).get('gutenberg')
-
-            file_type = re.search(r'(\.[a-zA-Z0-9]+)$', cover_data.get('image_path')).group(1)
-            cover_path = 'covers/gutenberg/{}{}'.format(gutenberg_id, file_type)
-            cover_url = get_stored_file_url(self.file_bucket, cover_path)
-
-            gutenberg_record.record.has_part.append(str(Part(
-                index=None,
-                source=Source.GUTENBERG.value,
-                url=cover_url,
-                file_type=mime_type,
-                flags=str(FileFlags(cover=True))
-            )))
-
-            cover_root = yaml_file.get('url').replace('ebooks', 'files')
-            cover_source_url = f"{cover_root}/{cover_data.get('image_path')}"
-
-            self.record_file_saver.store_file(file_url=cover_source_url, file_path=cover_path)
+                try:
+                    self.record_file_saver.store_file(file_url=part.url, file_path=epub_path)
+                except Exception:
+                    logger.info(f'Failed to save files for {gutenberg_record.record}')
