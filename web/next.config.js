c21dbfef6421795c3fb77eb9a7a8ff9489e77fa3
samanthaandrews <samantharaeandrews@gmail.com>
2025-03-31 12:54:32 -0400

    revert change to next config

--- web/next.config.js
+++ web/next.config.js
@@ -9,21 +9,21 @@ const nextConfig = {
   webpack: (config, { isServer }) => {
     config.resolve.alias["~"] = path.resolve(__dirname);
 
-    // if (process.env.APP_ENV === "testing") {
-    if (isServer) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        "msw/browser": false,
-      };
-    } else {
-      // Setting `resolve.alias` to `false` will tell webpack to ignore a module.
-      // `msw/node` is a server-only module that exports methods not available in
-      // the `browser`.
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        "msw/node": false,
-      };
-      // }
+    if (process.env.APP_ENV === "testing") {
+      if (isServer) {
+        config.resolve.alias = {
+          ...config.resolve.alias,
+          "msw/browser": false,
+        };
+      } else {
+        // Setting `resolve.alias` to `false` will tell webpack to ignore a module.
+        // `msw/node` is a server-only module that exports methods not available in
+        // the `browser`.
+        config.resolve.alias = {
+          ...config.resolve.alias,
+          "msw/node": false,
+        };
+      }
     }
 
     // In order for newrelic to effectively instrument a Next.js application,
